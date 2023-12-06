56501f78cf2bb971cf54373d51d500bb556bb666
superctj <congtj@umich.edu>
2023-12-06 04:25:48 +0000

    add postgres client

--- cybernetics/db_interface/postgres.py
+++ cybernetics/db_interface/postgres.py
@@ -1,4 +1,4 @@
-"""This module is adapted from DBTune"s dbconnector.py and postgresqldb.py.
+"""This module is adapted from DBTune's dbconnector.py and postgresqldb.py.
 
 https://github.com/PKU-DAIR/DBTune/blob/main/autotune/dbconnector.py
 https://github.com/PKU-DAIR/DBTune/blob/main/autotune/database/postgresqldb.py
@@ -12,17 +12,17 @@ import time
 import paramiko
 import psycopg2
 
-from cybernetics.db_interface.db_connector import DBConnector
+from cybernetics.db_interface.db_client import DBClient
 
 
-RESTART_WAIT_TIME = 20 # 20 minutes
-TIMEOUT_CLOSE = 60
+RESTART_WAIT_TIME = 20 # 20 seconds
+TIMEOUT_CLOSE = 60 # 60 seconds
 
 logger = logging.getLogger("paramiko")
 logger.setLevel(logging.ERROR)
 
 
-class PostgresqlConnector(DBConnector):
+class PostgresClient(DBClient):
     def __init__(self, host, port, user, password, db_name, socket=None):
         super().__init__()
         self.host = host
@@ -36,7 +36,6 @@ class PostgresqlConnector(DBConnector):
             self.cursor = self.conn.cursor()
 
     def connect_db(self):
-        conn = False
         if self.socket:
             conn = psycopg2.connect(host=self.host,
                                     port=self.port,
@@ -58,9 +57,7 @@ class PostgresqlConnector(DBConnector):
         if self.conn:
             self.conn.close()
 
-    def fetch_results(self, sql, json=True):
-        results = None
-        
+    def fetch_results(self, sql, json=True):    
         if self.conn:
             self.cursor.execute(sql)
             results = self.cursor.fetchall()
@@ -85,9 +82,9 @@ class PostgresWrapper:
         self.db_name = db_config["db_name"]
         self.socket = db_config["socket"]
         self.pid = int(db_config["pid"])
-        self.pg_cnf = db_config["cnf"]
+        self.pg_cnf = db_config["pg_cnf"]
         self.pg_ctl = db_config["pg_ctl"]
-        self.pg_data = db_config["pgdata"]
+        self.pg_data = db_config["pg_data"]
         self.postgres = os.path.join(os.path.split(os.path.abspath(self.pg_ctl))[0], "postgres")
 
         # TODO: Add support for remote mode
@@ -102,7 +99,7 @@ class PostgresWrapper:
         self.PG_STAT_VIEWS_LOCAL_DATABASE = ["pg_stat_database", "pg_stat_database_conflicts"]
         self.PG_STAT_VIEWS_LOCAL_TABLE = ["pg_stat_user_tables", "pg_statio_user_tables"]
         self.PG_STAT_VIEWS_LOCAL_INDEX = ["pg_stat_user_indexes", "pg_statio_user_indexes"]
-        self.NUMERIC_METRICS = [ # counter
+        self.NUMERIC_METRICS = [
             # global
             "buffers_alloc", "buffers_backend", "buffers_backend_fsync", "buffers_checkpoint", "buffers_clean",
             "checkpoints_req", "checkpoints_timed", "checkpoint_sync_time", "checkpoint_write_time", "maxwritten_clean",
@@ -121,136 +118,129 @@ class PostgresWrapper:
             "idx_blks_hit", "idx_blks_read", "idx_scan", "idx_tup_fetch", "idx_tup_read"
         ]
     
-    def _start_postgres(self):
-        if self.remote_mode:
-            ssh = paramiko.SSHClient()
-            ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-            ssh.connect(self.host, username=self.ssh_user, pkey=self.pk,
-                        disabled_algorithms={"pubkeys": ["rsa-sha2-256", "rsa-sha2-512"]})
-
-            start_cmd = "{} --config_file={} -D {}".format(self.postgres, self.pgcnf, self.pgdata)
-            wrapped_cmd = "echo $$; exec " + start_cmd
-            _, start_stdout, _ = ssh.exec_command(wrapped_cmd)
-            self.pid = int(start_stdout.readline())
-
-            if self.isolation_mode:
-                cgroup_cmd = "sudo -S cgclassify -g memory,cpuset:server " + str(self.pid)
-                ssh_stdin, ssh_stdout, _ = ssh.exec_command(cgroup_cmd)
-                ssh_stdin.write(self.ssh_passwd + "\n")
-                ssh_stdin.flush()
-                ret_code = ssh_stdout.channel.recv_exit_status()
-                ssh.close()
-                if not ret_code:
-                    logger.info("add {} to memory,cpuset:server".format(self.pid))
-                else:
-                    logger.info("Failed: add {} to memory,cpuset:server".format(self.pid))
-        else:
-            proc = subprocess.Popen([self.postgres, "--config_file={}".format(self.pgcnf), "-D",  self.pgdata])
-            self.pid = proc.pid
-            if self.isolation_mode:
-                command = "sudo cgclassify -g memory,cpuset:server " + str(self.pid)
-                p = os.system(command)
-                if not p:
-                    logger.info("add {} to memory,cpuset:server".format(self.pid))
-                else:
-                    logger.info("Failed: add {} to memory,cpuset:server".format(self.pid))
+    def _start_postgres(self) -> bool:
+        proc = subprocess.Popen([self.postgres, f"--config_file={self.pg_cnf}", "-D",  self.pg_data])
+        self.pid = proc.pid
 
         count = 0
-        start_sucess = True
-        logger.info("wait for connection")
+        start_success = True
+        logger.info("Wait for Postgres connection...")
+
         while True:
             try:
-                dbc = PostgresqlConnector(host=self.host,
-                                          port=self.port,
-                                          user=self.user,
-                                          passwd=self.passwd,
-                                          name=self.dbname)
-                db_conn = dbc.conn
-                if db_conn.closed == 0:
-                    logger.info("Connected to PostgreSQL db")
-                    db_conn.close()
+                pg_client = PostgresClient(host=self.host,
+                                           port=self.port,
+                                           user=self.user,
+                                           password=self.password,
+                                           db_name=self.db_name)
+                pg_conn = pg_client.conn
+                if pg_conn.closed == 0:
+                    logger.info("Connected to Postgres.")
+                    pg_conn.close()
                     break
             except:
                 pass
 
             time.sleep(1)
-            count = count + 1
+            count += 1
             if count > 600:
-                start_sucess = False
-                logger.info("can not connect to DB")
+                start_success = False
+                logger.info("Can not connect to Postgres.")
                 clear_cmd = """ps -ef|grep postgres|grep -v grep|cut -c 9-15|xargs kill -9"""
-                subprocess.Popen(clear_cmd, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE,
-                                 close_fds=True)
-                logger.info("kill all postgres process")
+                subprocess.Popen(clear_cmd, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, close_fds=True)
+                logger.info("Kill all Postgres processes.")
                 break
 
-        logger.info("finish {} seconds waiting for connection".format(count))
-        logger.info("postgres --config_file={}".format(self.pgcnf))
-        logger.info("postgres is up")
-        return start_sucess
+        logger.info(f"Postgres is up! Waited {count} seconds for connection.")
+        return start_success
     
-    def _kill_postgres(self):
-        kill_cmd = "{} stop -D {}".format(self.pg_ctl, self.pg_data)
+    def _kill_postgres(self) -> None:
+        kill_cmd = f"{self.pg_ctl} stop -D {self.pg_data}"
         force_kill_cmd1 = "ps aux|grep '" + self.socket + "'|awk '{print $2}'|xargs kill -9"
-        force_kill_cmd2 = "ps aux|grep '" + self.pg_cnf + "|awk '{print $2}'|xargs kill -9"
-
-        # if self.remote_mode:
-        #     ssh = paramiko.SSHClient()
-        #     ssh.set_missing_host_key_policy(paramiko.AutoAddPolicy())
-        #     ssh.connect(self.host, username=self.ssh_user, pkey=self.pk,
-        #                 disabled_algorithms={"pubkeys": ["rsa-sha2-256", "rsa-sha2-512"]})
-        #     ssh_stdin, ssh_stdout, ssh_stderr = ssh.exec_command(kill_cmd)
-        #     ret_code = ssh_stdout.channel.recv_exit_status()
-        #     if ret_code == 0:
-        #         logger.info("Close db successfully")
-        #     else:
-        #         logger.info("Force close DB!")
-        #         ssh.exec_command(force_kill_cmd1)
-        #         ssh.exec_command(force_kill_cmd2)
-        #     ssh.close()
-        #     logger.info("postgresql is shut down remotely")
-        # else:
-        #     p_close = subprocess.Popen(kill_cmd, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE,
-        #                                close_fds=True)
-        #     try:
-        #         outs, errs = p_close.communicate(timeout=TIMEOUT_CLOSE)
-        #         ret_code = p_close.poll()
-        #         if ret_code == 0:
-        #             logger.info("Close db successfully")
-        #     except subprocess.TimeoutExpired:
-        #         logger.info("Force close!")
-        #         os.system(force_kill_cmd1)
-        #         os.system(force_kill_cmd2)
-        #     logger.info("postgresql is shut down")
+        force_kill_cmd2 = "ps aux|grep '" + self.pg_cnf + "'|awk '{print $2}'|xargs kill -9"
 
         p_close = subprocess.Popen(kill_cmd, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, close_fds=True)
+        
         try:
-            outs, errs = p_close.communicate(timeout=TIMEOUT_CLOSE)
+            _, _ = p_close.communicate(timeout=TIMEOUT_CLOSE)
             ret_code = p_close.poll()
             if ret_code == 0:
-                logger.info("Close db successfully")
+                logger.info("Close Postgres successfully.")
         except subprocess.TimeoutExpired:
-            logger.info("Force close!")
+            logger.info("Force close Postgres.")
             os.system(force_kill_cmd1)
             os.system(force_kill_cmd2)
-        logger.info("postgresql is shut down")
+            logger.info("Postgres is shut down")
     
-    def apply_knobs_online(self, knobs) -> bool:
-        # apply knobs remotely
-        db_conn = PostgresqlConnector(host=self.host,
-                                      port=self.port,
-                                      user=self.user,
-                                      password=self.password,
-                                      db_name=self.db_name)
+    def _check_apply(self, db_conn, k, v0):
+        sql = f"SHOW {k};"
+        r = db_conn.fetch_results(sql)
+
+        if r[0]["Value"] == "ON":
+            vv = 1
+        elif r[0]["Value"] == "OFF":
+            vv = 0
+        else:
+            vv = r[0]["Value"].strip()
+
+        if vv == v0: # Knob value v has not taken effect yet
+            return False
+        else:
+            return True
+    
+    def set_knob_value(self, db_conn, k, v) -> bool:
+        sql = f"SHOW {k};"
+        r = db_conn.fetch_results(sql)
+
+        # Compare the value in the database with the value to be set
+        if r[0]["Value"] == "ON":
+            v0 = 1
+        elif r[0]["Value"] == "OFF":
+            v0 = 0
+        else:
+            try:
+                v0 = eval(r[0]["Value"])
+            except:
+                v0 = r[0]["Value"].strip()
+        
+        if v == "ON" or v == "on":
+            v = 1
+        elif v == "OFF" or v == "off":
+            v = 0
+        
+        if v0 == v:
+            return True
+
+        if str(v).isdigit():
+            sql = f"SET {k}={v};"
+        else:
+            sql = f"SET {k}='{v}';"
+        
+        try:
+            db_conn.execute(sql)
+        except:
+            logger.info(f"Failed to execute {sql}")
+
+        # Check if knob value v has taken effect
+        while not self._check_apply(db_conn, k, v0):
+            time.sleep(1)
+        
+        return True
+    
+    def apply_knobs_online(self, knobs):
+        pg_client = PostgresClient(host=self.host,
+                                   port=self.port,
+                                   user=self.user,
+                                   password=self.password,
+                                   db_name=self.db_name)
 
         for key in knobs.keys():
-            self.set_knob_value(db_conn, key, knobs[key])
+            self.set_knob_value(pg_client, key, knobs[key])
 
-        db_conn.close_db()
-        logger.info("[{}] Knobs applied online!".format(time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())))
-        return True
+        pg_client.close_db()
+        logger.info(f"[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}] Knobs applied online!")
 
-    def apply_knobs_offline(self, knobs) -> bool:
+    def apply_knobs_offline(self, knobs: dict) -> bool:
         self._kill_postgres()
 
         if "min_wal_size" in knobs.keys():
@@ -258,23 +248,26 @@ class PostgresWrapper:
                 wal_segment_size = knobs["wal_segment_size"]
             else:
                 wal_segment_size = 16
+            
             if knobs["min_wal_size"] < 2 * wal_segment_size:
                 knobs["min_wal_size"] = 2 * wal_segment_size
-                logger.info(""min_wal_size" must be at least twice "wal_segment_size"")
+                logger.info("min_wal_size must be at least twice wal_segment_size")
 
-        knobs_not_in_cnf = self._gen_config_file(knobs)
-        sucess = self._start_postgres()
+        # knobs_not_in_cnf = self._gen_config_file(knobs)
+        
+        success = self._start_postgres()
         try:
-            logger.info("sleeping for {} seconds after restarting postgres".format(RESTART_WAIT_TIME))
+            logger.info(f"Sleep for {RESTART_WAIT_TIME} seconds after restarting Postgres...")
             time.sleep(RESTART_WAIT_TIME)
 
-            if len(knobs_not_in_cnf) > 0:
-                tmp_rds = {}
-                for knob_rds in knobs_not_in_cnf:
-                    tmp_rds[knob_rds] = knobs[knob_rds]
-                self.apply_knobs_online(tmp_rds)
+            self.apply_knobs_online(knobs)
+
+            # if len(knobs_not_in_cnf) > 0:
+            #     tmp_rds = {}
+            #     for knob_rds in knobs_not_in_cnf:
+            #         tmp_rds[knob_rds] = knobs[knob_rds]
+            #     self.apply_knobs_online(tmp_rds)
         except:
-            sucess = False
+            success = False
 
-        return sucess
-
+        return success
