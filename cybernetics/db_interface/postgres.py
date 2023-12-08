00a20143fc129f0608299aed5f8dba0ef94b59bf
superctj <congtj@umich.edu>
2023-12-08 03:03:29 +0000

    add support of evaluating any given dbms configurations

--- cybernetics/db_interface/postgres.py
+++ cybernetics/db_interface/postgres.py
@@ -4,7 +4,7 @@ https://github.com/PKU-DAIR/DBTune/blob/main/autotune/dbconnector.py
 https://github.com/PKU-DAIR/DBTune/blob/main/autotune/database/postgresqldb.py
 """
 
-import logging
+import glob
 import os
 import subprocess
 import time
@@ -13,13 +13,11 @@ import paramiko
 import psycopg2
 
 from cybernetics.db_interface.db_client import DBClient
+from cybernetics.utils.custom_logging import CUSTOM_LOGGING_INSTANCE
 
 
 RESTART_WAIT_TIME = 20 # 20 seconds
 TIMEOUT_CLOSE = 60 # 60 seconds
-
-logger = logging.getLogger("paramiko")
-logger.setLevel(logging.ERROR)
 
 
 class PostgresClient(DBClient):
@@ -34,6 +32,8 @@ class PostgresClient(DBClient):
         self.conn = self.connect_db()
         if self.conn:
             self.cursor = self.conn.cursor()
+        
+        self.logger = CUSTOM_LOGGING_INSTANCE.get_module_logger(__name__)
 
     def connect_db(self):
         if self.socket:
@@ -74,18 +74,20 @@ class PostgresClient(DBClient):
 
 
 class PostgresWrapper:
-    def __init__(self, db_config) -> None:
+    def __init__(self, db_config, workload_wrapper, results_dir) -> None:
         self.host = db_config["host"]
         self.port = db_config["port"]
         self.user = db_config["user"]
         self.password = db_config["password"]
         self.db_name = db_config["db_name"]
-        self.socket = db_config["socket"]
-        self.pid = int(db_config["pid"])
-        self.pg_cnf = db_config["pg_cnf"]
-        self.pg_ctl = db_config["pg_ctl"]
-        self.pg_data = db_config["pg_data"]
-        self.postgres = os.path.join(os.path.split(os.path.abspath(self.pg_ctl))[0], "postgres")
+        # self.socket = db_config["socket"]
+        # self.pid = int(db_config["pid"])
+        # self.pg_cnf = db_config["pg_cnf"]
+        # self.pg_ctl = db_config["pg_ctl"]
+        # self.pg_data = db_config["pg_data"]
+        # self.postgres = os.path.join(os.path.split(os.path.abspath(self.pg_ctl))[0], "postgres")
+        self.workload_wrapper = workload_wrapper
+        self.results_dir = results_dir
 
         # TODO: Add support for remote mode
 
@@ -124,7 +126,7 @@ class PostgresWrapper:
 
         count = 0
         start_success = True
-        logger.info("Wait for Postgres connection...")
+        self.logger.info("Wait for Postgres connection...")
 
         while True:
             try:
@@ -135,7 +137,7 @@ class PostgresWrapper:
                                            db_name=self.db_name)
                 pg_conn = pg_client.conn
                 if pg_conn.closed == 0:
-                    logger.info("Connected to Postgres.")
+                    self.logger.info("Connected to Postgres.")
                     pg_conn.close()
                     break
             except:
@@ -145,13 +147,13 @@ class PostgresWrapper:
             count += 1
             if count > 600:
                 start_success = False
-                logger.info("Can not connect to Postgres.")
+                self.logger.info("Can not connect to Postgres.")
                 clear_cmd = """ps -ef|grep postgres|grep -v grep|cut -c 9-15|xargs kill -9"""
                 subprocess.Popen(clear_cmd, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, close_fds=True)
-                logger.info("Kill all Postgres processes.")
+                self.logger.info("Kill all Postgres processes.")
                 break
 
-        logger.info(f"Postgres is up! Waited {count} seconds for connection.")
+        self.logger.info(f"Postgres is up! Waited {count} seconds for connection.")
         return start_success
     
     def _kill_postgres(self) -> None:
@@ -165,12 +167,12 @@ class PostgresWrapper:
             _, _ = p_close.communicate(timeout=TIMEOUT_CLOSE)
             ret_code = p_close.poll()
             if ret_code == 0:
-                logger.info("Close Postgres successfully.")
+                self.logger.info("Close Postgres successfully.")
         except subprocess.TimeoutExpired:
-            logger.info("Force close Postgres.")
+            self.logger.info("Force close Postgres.")
             os.system(force_kill_cmd1)
             os.system(force_kill_cmd2)
-            logger.info("Postgres is shut down")
+            self.logger.info("Postgres is shut down")
     
     def _check_apply(self, db_conn, k, v0):
         sql = f"SHOW {k};"
@@ -219,7 +221,7 @@ class PostgresWrapper:
         try:
             db_conn.execute(sql)
         except:
-            logger.info(f"Failed to execute {sql}")
+            self.logger.info(f"Failed to execute {sql}")
 
         # Check if knob value v has taken effect
         while not self._check_apply(db_conn, k, v0):
@@ -238,7 +240,7 @@ class PostgresWrapper:
             self.set_knob_value(pg_client, key, knobs[key])
 
         pg_client.close_db()
-        logger.info(f"[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}] Knobs applied online!")
+        self.logger.info(f"[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}] Knobs applied online!")
 
     def apply_knobs_offline(self, knobs: dict) -> bool:
         self._kill_postgres()
@@ -251,13 +253,13 @@ class PostgresWrapper:
             
             if knobs["min_wal_size"] < 2 * wal_segment_size:
                 knobs["min_wal_size"] = 2 * wal_segment_size
-                logger.info("min_wal_size must be at least twice wal_segment_size")
+                self.logger.info("min_wal_size must be at least twice wal_segment_size")
 
         # knobs_not_in_cnf = self._gen_config_file(knobs)
         
         success = self._start_postgres()
         try:
-            logger.info(f"Sleep for {RESTART_WAIT_TIME} seconds after restarting Postgres...")
+            self.logger.info(f"Sleep for {RESTART_WAIT_TIME} seconds after restarting Postgres...")
             time.sleep(RESTART_WAIT_TIME)
 
             self.apply_knobs_online(knobs)
@@ -271,3 +273,22 @@ class PostgresWrapper:
             success = False
 
         return success
+
+    def _get_benchbase_metrics(self):
+        metrics_files = glob.glob(f"{self.results_dir}/*.summary.json")
+        latest_metrics_file = max(metrics_files, key=os.path.getctime)
+        pass
+
+    def evaluate_db_config(self, knobs: dict) -> dict:
+        success = self.apply_knobs_offline(knobs)
+        if not success:
+            self.logger.info("Failed to apply the given DBMS configuration.")
+            return {}
+        
+        # Execute the workload
+        self.workload_wrapper.run()
+
+        # Collect metrics
+        metrics = self._get_benchbase_metrics()
+
+        return metrics
