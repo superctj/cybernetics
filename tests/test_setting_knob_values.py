9a9c418fccf34b655bfb092f49f3550329dcd1a4
superctj <congtj@umich.edu>
2023-12-12 02:08:09 +0000

    test setting knob values

--- tests/test_setting_knob_values.py
+++ tests/test_setting_knob_values.py
@@ -7,33 +7,20 @@ def test_setting_knob_values():
         "port": 5432,
         "user": "postgres",
         "password": "12345",
+        "db_cluster": "/data/pgsql/data",
+        "db_log_filepath": "/data/pgsql/log",
         "db_name": "benchbase_tpcc" # This database should be created beforehand
     }
 
     postgres_wrapper = PostgresWrapper(db_info, workload_wrapper=None, results_dir=None)
     print("\n    Created Postgres wrapper.")
 
-    postgres_client = PostgresClient(db_info["host"], db_info["port"], db_info["user"], db_info["password"], db_info["db_name"])
+    knob_to_set = "shared_buffers"
+    db_config = {knob_to_set: "1 GB"} # default is 128 MB
+    default_val = postgres_wrapper.get_knob_value(knob_to_set)
+    print(f"    Default value of {knob_to_set}: {default_val}")
 
-    db_config = {"work_mem": "1GB"} # default is 4MB
-    for key in db_config:
-        knob_val_sql = f"SHOW {key};"
-        default_val = postgres_client.execute_and_fetch_results(
-            knob_val_sql, json=False)[0][0]
-        # print(f"    {key} default value:", default_val)
-        
-        # Apply the knob value
-        # set_knob_sql = f"SET {key} = '{db_config[key]}';"
-        # predicate = postgres_client.execute(set_knob_sql)
-        # assert(predicate == True)
-
-        # cur_val = postgres_client.execute_and_fetch_results(
-        #     knob_val_sql, json=False)[0][0]
-        # assert(cur_val == db_config[key])
-        predicate = postgres_wrapper.set_knob_value(postgres_client, key, db_config[key])
-        assert(predicate == True)
-
-        # Set the knob value back to default
-        set_knob_sql = f"SET {key} = '{default_val}';"
-        predicate = postgres_client.execute(set_knob_sql)
-        assert(predicate == True)
+    rtn_predicate = postgres_wrapper.apply_knobs(db_config)
+    cur_val = postgres_wrapper.get_knob_value(knob_to_set)
+    print(f"    Current value of {knob_to_set}: {cur_val}")
+    assert rtn_predicate == True
