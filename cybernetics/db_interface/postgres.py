9ffcb31ea40db311ba776b6c4f8de88edb26cb3c
superctj <congtj@umich.edu>
2023-12-12 02:09:38 +0000

    fix the issue of ALTER SYSTEM not taking effect

--- cybernetics/db_interface/postgres.py
+++ cybernetics/db_interface/postgres.py
@@ -10,26 +10,25 @@ import os
 import subprocess
 import time
 
-import paramiko
+# import paramiko
 import psycopg2
 
 from cybernetics.db_interface.db_client import DBClient
 from cybernetics.utils.custom_logging import CUSTOM_LOGGING_INSTANCE
 
 
-RESTART_WAIT_TIME = 20 # 20 seconds
-TIMEOUT_CLOSE = 60 # 60 seconds
+RESTART_TIMEOUT = 60 # 60 seconds
 
 
 class PostgresClient(DBClient):
-    def __init__(self, host, port, user, password, db_name):
+    def __init__(self, host, port, user, password, db_name, logger):
         super().__init__()
         self.host = host
         self.port = port
         self.user = user
         self.password = password
         self.db_name = db_name
-        self.logger = CUSTOM_LOGGING_INSTANCE.get_module_logger(__name__)
+        self.logger = logger
         
         self.conn, self.cursor = self.connect_db()
 
@@ -40,7 +39,7 @@ class PostgresClient(DBClient):
                                     user=self.user,
                                     password=self.password,
                                     database=self.db_name)
-            
+            conn.autocommit = True
             cursor = conn.cursor()
             self.logger.info("Connected to Postgres.")
             return conn, cursor
@@ -77,12 +76,15 @@ class PostgresClient(DBClient):
 
 
 class PostgresWrapper:
-    def __init__(self, db_info, workload_wrapper, results_dir) -> None:
+    def __init__(self, db_info, workload_wrapper=None, results_dir=None) -> None:
         self.host = db_info["host"]
         self.port = db_info["port"]
         self.user = db_info["user"]
         self.password = db_info["password"]
+        self.db_cluster = db_info["db_cluster"]
+        self.db_log_filepath = db_info["db_log_filepath"]
         self.db_name = db_info["db_name"]
+        
         self.workload_wrapper = workload_wrapper
         self.results_dir = results_dir
         self.logger = CUSTOM_LOGGING_INSTANCE.get_module_logger(__name__)
@@ -117,59 +119,78 @@ class PostgresWrapper:
             "idx_blks_hit", "idx_blks_read", "idx_scan", "idx_tup_fetch", "idx_tup_read"
         ]
     
-    def _start_postgres(self) -> bool:
-        proc = subprocess.Popen([self.postgres, f"--config_file={self.pg_cnf}", "-D",  self.pg_data])
-        self.pid = proc.pid
+    # def _start_postgres(self) -> bool:
+    #     proc = subprocess.Popen([self.postgres, f"--config_file={self.pg_cnf}", "-D",  self.pg_data])
+    #     self.pid = proc.pid
 
-        count = 0
-        start_success = True
-        self.logger.info("Wait for Postgres connection...")
+    #     count = 0
+    #     start_success = True
+    #     self.logger.info("Wait for Postgres connection...")
 
-        while True:
-            try:
-                pg_client = PostgresClient(host=self.host,
-                                           port=self.port,
-                                           user=self.user,
-                                           password=self.password,
-                                           db_name=self.db_name)
-                if pg_client.conn.closed == 0:
-                    self.logger.info("Connected to Postgres.")
-                    pg_client.close_connection()
-                    break
-            except:
-                pass
+    #     while True:
+    #         try:
+    #             pg_client = PostgresClient(host=self.host,
+    #                                        port=self.port,
+    #                                        user=self.user,
+    #                                        password=self.password,
+    #                                        db_name=self.db_name)
+    #             if pg_client.conn.closed == 0:
+    #                 self.logger.info("Connected to Postgres.")
+    #                 pg_client.close_connection()
+    #                 break
+    #         except:
+    #             pass
 
-            time.sleep(1)
-            count += 1
-            if count > 600:
-                start_success = False
-                self.logger.info("Can not connect to Postgres.")
-                clear_cmd = """ps -ef|grep postgres|grep -v grep|cut -c 9-15|xargs kill -9"""
-                subprocess.Popen(clear_cmd, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, close_fds=True)
-                self.logger.info("Kill all Postgres processes.")
-                break
+    #         time.sleep(1)
+    #         count += 1
+    #         if count > 600:
+    #             start_success = False
+    #             self.logger.info("Can not connect to Postgres.")
+    #             clear_cmd = """ps -ef|grep postgres|grep -v grep|cut -c 9-15|xargs kill -9"""
+    #             subprocess.Popen(clear_cmd, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, close_fds=True)
+    #             self.logger.info("Kill all Postgres processes.")
+    #             break
 
-        self.logger.info(f"Postgres is up! Waited {count} seconds for connection.")
-        return start_success
+    #     self.logger.info(f"Postgres is up! Waited {count} seconds for connection.")
+    #     return start_success
     
-    def _kill_postgres(self) -> None:
-        kill_cmd = f"{self.pg_ctl} stop -D {self.pg_data}"
-        force_kill_cmd1 = "ps aux|grep '" + self.socket + "'|awk '{print $2}'|xargs kill -9"
-        force_kill_cmd2 = "ps aux|grep '" + self.pg_cnf + "'|awk '{print $2}'|xargs kill -9"
+    # def _kill_postgres(self) -> None:
+    #     kill_cmd = f"{self.pg_ctl} stop -D {self.pg_data}"
+    #     force_kill_cmd1 = "ps aux|grep '" + self.socket + "'|awk '{print $2}'|xargs kill -9"
+    #     force_kill_cmd2 = "ps aux|grep '" + self.pg_cnf + "'|awk '{print $2}'|xargs kill -9"
 
-        p_close = subprocess.Popen(kill_cmd, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, close_fds=True)
+    #     p_close = subprocess.Popen(kill_cmd, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, close_fds=True)
         
+    #     try:
+    #         _, _ = p_close.communicate(timeout=TIMEOUT_CLOSE)
+    #         ret_code = p_close.poll()
+    #         if ret_code == 0:
+    #             self.logger.info("Close Postgres successfully.")
+    #     except subprocess.TimeoutExpired:
+    #         self.logger.info("Force close Postgres.")
+    #         os.system(force_kill_cmd1)
+    #         os.system(force_kill_cmd2)
+    #         self.logger.info("Postgres is shut down")
+
+    def _restart_postgres(self) -> bool:
+        payload = ["pg_ctl", "-D", self.db_cluster, "-l", self.db_log_filepath, "restart"]
+        p = subprocess.Popen(payload, stderr=subprocess.PIPE, 
+                             stdout=subprocess.PIPE, close_fds=True)
         try:
-            _, _ = p_close.communicate(timeout=TIMEOUT_CLOSE)
-            ret_code = p_close.poll()
-            if ret_code == 0:
-                self.logger.info("Close Postgres successfully.")
+            # communicate() will block the program until the subprocess finishes
+            stdout, stderr = p.communicate(timeout=RESTART_TIMEOUT)
+            if p.returncode == 0:
+                self.logger.info("Restarted Postgres.")
+                self.logger.info(f"Subprocess output: \n{stdout.decode()}")
+                return True
+            else:
+                self.logger.info("Failed to restart Postgres.")
+                self.logger.info(f"Subprocess output: \n{stderr.decode()}")
+                return False
         except subprocess.TimeoutExpired:
-            self.logger.info("Force close Postgres.")
-            os.system(force_kill_cmd1)
-            os.system(force_kill_cmd2)
-            self.logger.info("Postgres is shut down")
-    
+            self.logger.info("Timeout when restarting Postgres.")
+            return False
+
     def _check_applied(self, db_conn, k, default_val):
         sql = f"SHOW {k};"
         cur_val = db_conn.execute_and_fetch_results(sql)[0][0]
@@ -179,9 +200,27 @@ class PostgresWrapper:
         else:
             return True
     
-    def set_knob_value(self, db_conn, k, v) -> bool:
+    def get_knob_value(self, k) -> str:
+        try:
+            pg_client = PostgresClient(host=self.host,
+                                       port=self.port,
+                                       user=self.user,
+                                       password=self.password,
+                                       db_name=self.db_name,
+                                       logger=self.logger)
+            
+            get_val_sql = f"SHOW {k};"
+            val = pg_client.execute_and_fetch_results(get_val_sql, json=False)[0][0]
+            pg_client.close_connection()
+
+            return val
+        except:
+            self.logger.info(f"Failed to get value of knob {k}.")
+            return None
+    
+    def set_knob_value(self, db_client, k, v) -> bool:
         sql = f"SHOW {k};"
-        cur_val = db_conn.execute_and_fetch_results(sql, json=False)[0][0]
+        cur_val = db_client.execute_and_fetch_results(sql, json=False)[0][0]
         self.logger.info(f"Current value of knob {k}: {cur_val}")
 
         if cur_val == v:
@@ -189,13 +228,19 @@ class PostgresWrapper:
             return True
         else:
             if type(v) == str:
-                set_sql = f"SET {k}='{v}';"
+                # set_sql = f"SET {k} = '{v}';"
+                set_sql = f"ALTER SYSTEM SET {k}='{v}';"
             else:
                 assert type(v) == int
-                set_sql = f"SET {k}={v};"
-            
+                # set_sql = f"SET {k} = {v};"
+                set_sql = f"ALTER SYSTEM SET {k}={v};"
+
             try:
-                _ = db_conn.execute(set_sql)
+                # reload_sql = "SELECT pg_reload_conf();"
+                _ = db_client.execute(set_sql)
+                self.logger.info(f"Set knob {k} to {v}.")
+
+                # _ = db_conn.execute(reload_sql)
                 # Check if knob value v has taken effect
                 # while not self._check_applied(db_conn, k, cur_val):
                 #     time.sleep(1)
@@ -204,22 +249,123 @@ class PostgresWrapper:
                 self.logger.info(f"Failed to set knob {k} to {v}.")
                 return False
     
-    def apply_knobs_online(self, knobs: dict) -> bool:
+    def reset_knob_value(self, k) -> bool:
+        """Restore the value of a run-time knob to the default value
+        """
         try:
             pg_client = PostgresClient(host=self.host,
                                        port=self.port,
                                        user=self.user,
                                        password=self.password,
-                                       db_name=self.db_name)
-
-            for key in knobs.keys():
-                self.set_knob_value(pg_client, key, knobs[key])
-
+                                       db_name=self.db_name,
+                                       logger=self.logger)
+            reset_sql = f"ALTER SYSTEM RESET {k};"
+            reload_sql = "SELECT pg_reload_conf();"
+            _ = pg_client.execute(reset_sql)
+            _ = pg_client.execute(reload_sql)
+            
             pg_client.close_connection()
-            self.logger.info(f"[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}] Knobs applied online.")
-
             return True
         except:
+            self.logger.info(f"Failed to reset knob {k} to the default value.")
+            return False
+
+    # def reset_knobs_online(self, knobs: list) -> bool:
+    #     try:
+    #         pg_client = PostgresClient(host=self.host,
+    #                                    port=self.port,
+    #                                    user=self.user,
+    #                                    password=self.password,
+    #                                    db_name=self.db_name)
+    #         for k in knobs:
+    #             self.reset_knob_value(pg_client, k)
+
+    #         pg_client.close_connection()
+    #         self.logger.info(f"[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}] Knobs applied online.")
+
+    #         return True
+    #     except:
+    #         return False
+    
+    def apply_knobs(self, knobs: dict) -> bool:
+        try:
+            pg_client = PostgresClient(host=self.host,
+                                       port=self.port,
+                                       user=self.user,
+                                       password=self.password,
+                                       db_name=self.db_name,
+                                       logger=self.logger)
+            
+            # TODO: Set all knob values before restarting Postgres
+            for key in knobs.keys():
+                self.set_knob_value(pg_client, key, knobs[key])
+            
+            pg_client.close_connection()
+            self._restart_postgres()
+            self.logger.info(f"Knobs applied.")
+            return True
+        except:
+            return False
+    
+    def reset_runtime_knobs(self) -> bool:
+        try:
+            pg_client = PostgresClient(host=self.host,
+                                       port=self.port,
+                                       user=self.user,
+                                       password=self.password,
+                                       db_name=self.db_name,
+                                       logger=self.logger)
+            
+            reset_sql = "RESET ALL;"
+            _ = pg_client.execute(reset_sql)
+            pg_client.close_connection()
+
+            self.logger.info(f"Reset runtime knobs.")
+            return True
+        except:
+            self.logger.info("Failed to reset runtime knobs.")
+            return False
+    
+    def reset_knobs_by_reloading_config(self) -> bool:
+        try:
+            pg_client = PostgresClient(host=self.host,
+                                       port=self.port,
+                                       user=self.user,
+                                       password=self.password,
+                                       db_name=self.db_name,
+                                       logger=self.logger)
+            
+            reset_sql = "ALTER SYSTEM RESET ALL;"
+            reload_sql = "SELECT pg_reload_conf();"
+
+            _ = pg_client.execute(reset_sql)
+            _ = pg_client.execute(reload_sql)
+            pg_client.close_connection()
+
+            self.logger.info(f"Reset runtime knobs.")
+            return True
+        except:
+            self.logger.info("Failed to reset runtime knobs.")
+            return False
+    
+    def reset_knobs_by_restarting_db(self) -> bool:
+        try:
+            pg_client = PostgresClient(host=self.host,
+                                       port=self.port,
+                                       user=self.user,
+                                       password=self.password,
+                                       db_name=self.db_name,
+                                       logger=self.logger)
+            
+            reset_sql = "ALTER SYSTEM RESET ALL;"
+            _ = pg_client.execute(reset_sql)
+            pg_client.close_connection()
+
+            self._restart_postgres()
+            self.logger.info(f"Reset knobs with Postgres restarted.")
+            return True
+        except:
+            self.logger.info("Failed to reset knobs with Postgres restarted.")
             return False
 
     def apply_knobs_offline(self, knobs: dict) -> bool:
@@ -239,8 +385,8 @@ class PostgresWrapper:
         
         success = self._start_postgres()
         try:
-            self.logger.info(f"Sleep for {RESTART_WAIT_TIME} seconds after restarting Postgres...")
-            time.sleep(RESTART_WAIT_TIME)
+            self.logger.info(f"Sleep for {RESTART_TIMEOUT} seconds after restarting Postgres...")
+            time.sleep(RESTART_TIMEOUT)
 
             self.apply_knobs_online(knobs)
 
