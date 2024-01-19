0cc62dbad580cce9d8ed88a7e28572acd468d5b8
superctj <congtj@umich.edu>
2024-01-19 04:32:30 +0000

    add exp tracker, target function, and optimizer initializer

--- cybernetics/tuning/engine.py
+++ cybernetics/tuning/engine.py
@@ -6,40 +6,69 @@ https://github.com/PKU-DAIR/DBTune/blob/main/autotune/tuner.py
 https://github.com/uw-mad-dash/llamatune/blob/main/run-smac.py
 """
 
+from cybernetics.tuning.dbms_config_optimizer import get_bo_optimizer
 from cybernetics.utils.custom_logging import CUSTOM_LOGGING_INSTANCE
+from cybernetics.utils.exp_tracker import ExperimentState
+
+
+logger = CUSTOM_LOGGING_INSTANCE.get_module_logger(__name__)
 
 
 class TuningEngine:
-    def __init__(self, db_wrapper, db_config_space, optimizer, exp_state) -> None:
-        self.db_wrapper = db_wrapper
-        self.db_config_space = db_config_space
-        self.optimizer = optimizer
-        self.exp_state = exp_state
-        self.logger = CUSTOM_LOGGING_INSTANCE.get_module_logger(__name__)
+    def __init__(self, config, dbms_wrapper, dbms_config_space, workload_wrapper) -> None:
+        self.config = config
+        self.dbms_wrapper = dbms_wrapper
+        self.dbms_config_space = dbms_config_space
+        self.workload_wrapper = workload_wrapper
 
-    def _evaluate_default_db_config(self):
-        """Evaluate the default DBMS configuration.
+        self.exp_state = ExperimentState(
+            config["dbms_info"],
+            config["workload_info"],
+            config["config_optimizer"]["target_metric"]
+        )
+        self.target_metric = self.config["config_optimizer"]["target_metric"]
+        self.optimizer = self.init_optimizer()
+        
+
+    def target_function(self, dbms_config):
+        """Target function for the DBMS configuration optimizer.
         """
-        default_dbms_config = self.knob_space.get_default_configuration()
-        self.logger.info("Evaluating default DBMS configuration...")
-        self.logger.info(f"Default DBMS config: {default_dbms_config}")
+        rtn_predicate = self.dbms_wrapper.apply_knobs(dbms_config)
+        assert(rtn_predicate, "Failed to apply DBMS configuration.")
 
-        default_perf = self.db_wrapper.evaluate_db_config(default_dbms_config)
-        default_perf = default_perf if self.exp_state.minimize else -default_perf
-        assert default_perf >= 0, f"Performance should not be negative: perf={default_perf}, metric={self.exp_state.target_metric}"
+        self.workload_wrapper.run()
+        performance = self.dbms_wrapper.get_benchbase_metrics()
 
-        self.exp_state.worse_perf = default_perf
+        if self.target_metric == "throughput":
+            throughput = performance["Throughput (requests/second)"]
+            logger.info(f"Throughput (requests/second): {throughput}")
+            
+            if self.exp_state.best_perf is None or throughput > self.exp_state.best_perf:
+                self.exp_state.best_perf = throughput
+            return -throughput
+        elif self.target_metric == "latency":
+            latency = performance["Latency Distribution"]["95th Percentile Latency (microseconds)"]
+            logger.info(f"95th Percentile Latency (microseconds): {latency}")
+
+            if self.exp_state.best_perf is None or latency < self.exp_state.best_perf:
+                self.exp_state.best_perf = latency
+            return latency
+    
+    def init_optimizer(self):
+        if self.config["config_optimizer"]["optimizer"].starts_with("bo"):
+            return get_bo_optimizer(self.config, self.dbms_config_space, self.target_function)
 
     def run(self):
         # SMAC
         if hasattr(self.optimizer, "optimize"):
-            self.optimizer.optimize()
+            best_dbms_config = self.optimizer.optimize()
         # OpenBox
         else:
             self.optimizer.run()
         
         # Complete tuning
-        self.logger.info(f"\nBest Configuration:\n{self.exp_state.best_conf}")
+        self.logger.info(f"\nBest DBMS Configuration:\n{best_dbms_config}")
+        
         if self.exp_state.target_metric == "throughput":
             self.logger.info(f"Throughput: {self.exp_state.best_perf} ops/sec")
         else:
