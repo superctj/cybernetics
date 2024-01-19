242c25c4177371ce05adbb64360525f4f7d29670
superctj <congtj@umich.edu>
2024-01-19 04:30:12 +0000

    move some util to tuning engine

--- run_smac.py
+++ run_smac.py
@@ -2,9 +2,7 @@ import argparse
 
 from cybernetics.tuning.engine import TuningEngine
 from cybernetics.knobs.generate_space import KnobSpaceGenerator
-from cybernetics.tuning.db_config_optimizer import get_smac_optimizer
 from cybernetics.utils.custom_logging import CUSTOM_LOGGING_INSTANCE, create_dir
-from cybernetics.utils.exp_tracker import ExperimentState
 from cybernetics.utils.util import fix_global_random_state, parse_config
 
 
@@ -17,55 +15,39 @@ if __name__ == "__main__":
         required=True,
         help="Path to the configuration file"
     )
-    parser.add_argument(
-        "--random_seed",
-        type=int,
-        default=12345,
-        required=True,
-        help="Random seed for deterministic experiments"
-    )
 
     args = parser.parse_args()
-    
-    # Set global random state
-    fix_global_random_state(args.random_seed)
 
     # Parse configuration
-    config_parser = parse_config(args.config_path)
+    config = parse_config(args.config_path)
+
+    # Set global random state
+    fix_global_random_state(config["config_optimizer"]["random_seed"])
 
     # Create logger for the main module
     # main_logger = CUSTOM_LOGGING_INSTANCE.get_module_logger(module_name=__name__)
 
-    # Create output directory to store DBMS execution results
-    create_dir(config_parser["results"]["save_path"], force=False)
-
     # Create workload wrapper
-    if config_parser["workload_info"]["benchmark"] == "benchbase":
+    if config["workload_info"]["benchmark"] == "benchbase":
         from cybernetics.workload.benchbase import BenchBaseWrapper
         workload_wrapper = BenchBaseWrapper(
-            config_parser["benchmark_info"]["workload"],
-            config_parser["benchmark_info"]["script"])
+            config["benchmark_info"]["workload"],
+            config["benchmark_info"]["script"])
     
     # Create DBMS executor
-    if config_parser["dbms_info"]["dbms_name"] == "postgres":
+    if config["dbms_info"]["dbms_name"] == "postgres":
         from cybernetics.db_interface.postgres import PostgresWrapper
-        postgres_wrapper = PostgresWrapper(config_parser["dbms_info"], 
+        postgres_wrapper = PostgresWrapper(config["dbms_info"], 
                                            workload_wrapper)
 
     # Init DBMS config space
-    db_config_space_generator = KnobSpaceGenerator(
-        config_parser["knob_space"]["knob_spec"], args.random_seed)
-    db_config_space = db_config_space_generator.generate_input_space(
+    dbms_config_space_generator = KnobSpaceGenerator(
+        config["knob_space"]["knob_spec"], args.random_seed)
+    dbms_config_space = dbms_config_space_generator.generate_input_space(
         ignored_knobs=[])
-
-    # Keep track of experiment state
-    exp_state = ExperimentState(config_parser["benchmark_info"], config_parser["dbms_info"], config_parser["optimizer"]["target_metric"])
-
-    # Init optimizer
-    optimizer = get_smac_optimizer(
-        config_parser, db_config_space, postgres_wrapper, exp_state)
     
-    # Run tuning
+    # Init tuning engine
     tuning_engine = TuningEngine(
-        postgres_wrapper, db_config_space, optimizer, exp_state)
+        config, postgres_wrapper, dbms_config_space, workload_wrapper
+    )
     tuning_engine.run()
