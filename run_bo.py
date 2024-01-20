ddb438155fc3d564bcea5b88e191e07080a233c4
superctj <congtj@umich.edu>
2024-01-20 21:41:18 +0000

    rename file

--- run_bo.py
+++ run_bo.py
@@ -2,7 +2,6 @@ import argparse
 
 from cybernetics.tuning.engine import TuningEngine
 from cybernetics.knobs.generate_space import KnobSpaceGenerator
-from cybernetics.utils.custom_logging import CUSTOM_LOGGING_INSTANCE, create_dir
 from cybernetics.utils.util import fix_global_random_state, parse_config
 
 
@@ -22,27 +21,24 @@ if __name__ == "__main__":
     config = parse_config(args.config_path)
 
     # Set global random state
-    fix_global_random_state(config["config_optimizer"]["random_seed"])
-
-    # Create logger for the main module
-    # main_logger = CUSTOM_LOGGING_INSTANCE.get_module_logger(module_name=__name__)
+    fix_global_random_state(int(config["knob_space"]["random_seed"]))
 
     # Create workload wrapper
-    if config["workload_info"]["benchmark"] == "benchbase":
+    if config["workload_info"]["framework"] == "benchbase":
         from cybernetics.workload.benchbase import BenchBaseWrapper
         workload_wrapper = BenchBaseWrapper(
-            config["benchmark_info"]["workload"],
-            config["benchmark_info"]["script"])
+            config["workload_info"]["workload"],
+            config["workload_info"]["script"])
     
     # Create DBMS executor
     if config["dbms_info"]["dbms_name"] == "postgres":
-        from cybernetics.db_interface.postgres import PostgresWrapper
+        from cybernetics.dbms_interface.postgres import PostgresWrapper
         postgres_wrapper = PostgresWrapper(config["dbms_info"], 
                                            workload_wrapper)
 
     # Init DBMS config space
     dbms_config_space_generator = KnobSpaceGenerator(
-        config["knob_space"]["knob_spec"], args.random_seed)
+        config["knob_space"]["knob_spec"], int(config["knob_space"]["random_seed"]))
     dbms_config_space = dbms_config_space_generator.generate_input_space(
         ignored_knobs=[])
     
