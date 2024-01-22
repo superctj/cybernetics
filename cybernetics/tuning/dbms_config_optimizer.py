9810a74f8c5f15a3c4983822d270d1aea0134b8d
superctj <congtj@umich.edu>
2024-01-22 04:17:09 +0000

    enable vanilla bo and smac

--- cybernetics/tuning/dbms_config_optimizer.py
+++ cybernetics/tuning/dbms_config_optimizer.py
@@ -10,7 +10,7 @@ from smac import BlackBoxFacade as BBFacade
 from smac import HyperparameterOptimizationFacade as HPOFacade
 from smac import Scenario
 
-from cybernetics.knobs.bias_sampling import LHDesignWithBiasedSampling
+# from cybernetics.knobs.bias_sampling import LHDesignWithBiasedSampling
 from cybernetics.utils.custom_logging import CUSTOM_LOGGING_INSTANCE
 
 
@@ -23,13 +23,15 @@ def get_bo_optimizer(config, dbms_config_space: ConfigurationSpace, target_funct
     # logger.info(f"Ignored knobs: {ignored_knobs}")
 
     scenario = Scenario(
-        config_space=dbms_config_space,
-        output_dir=config["results"]["save_path"],
+        configspace=dbms_config_space,
+        output_directory=config["results"]["save_path"],
         deterministic=True,
         objectives="cost", # minimize the objective
         n_trials=100,
-        seed=config["config_optimizer"]["random_seed"]
-    ) 
+        seed=int(config["knob_space"]["random_seed"])
+    )
+
+    target_function = partial(target_function, seed=int(config["knob_space"]["random_seed"]))
 
     if config["config_optimizer"]["optimizer"] == "bo-gp":
         optimizer = BBFacade(
@@ -47,82 +49,82 @@ def get_bo_optimizer(config, dbms_config_space: ConfigurationSpace, target_funct
     return optimizer
 
 
-def get_smac_optimizer(config, knob_space: ConfigurationSpace, tae_runner, state, ignored_knobs=None, run_history=None):
-    logger = CUSTOM_LOGGING_INSTANCE.get_module_logger(__name__)
-    logger.info("Constructing new optimizer...")
-    logger.info(f"Run History: {run_history}")
-    logger.info(f"Ignored knobs: {ignored_knobs}")
+# def get_smac_optimizer(config, knob_space: ConfigurationSpace, tae_runner, state, ignored_knobs=None, run_history=None):
+#     logger = CUSTOM_LOGGING_INSTANCE.get_module_logger(__name__)
+#     logger.info("Constructing new optimizer...")
+#     logger.info(f"Run History: {run_history}")
+#     logger.info(f"Ignored knobs: {ignored_knobs}")
 
-    # Generate input (i.e. knobs) and output (i.e. perf metric) space
-    input_space = knob_space.generate_input_space(
-        config.seed, ignore_extra_knobs=ignored_knobs)
+#     # Generate input (i.e. knobs) and output (i.e. perf metric) space
+#     input_space = knob_space.generate_input_space(
+#         config.seed, ignore_extra_knobs=ignored_knobs)
 
-    logger.info(f"\ninput space:\n{input_space}")
+#     logger.info(f"\ninput space:\n{input_space}")
 
-    scenario = Scenario({
-        "run_obj": "quality",
-        "runcount-limit": config.iters,
-        "cs": input_space,
-        "deterministic": "true",
-        "always_race_default": "false",
-        # disables pynisher, which allows for shared state
-        "limit_resources": "false",
-        "output_dir": state.results_path,
-    })
+#     scenario = Scenario({
+#         "run_obj": "quality",
+#         "runcount-limit": config.iters,
+#         "cs": input_space,
+#         "deterministic": "true",
+#         "always_race_default": "false",
+#         # disables pynisher, which allows for shared state
+#         "limit_resources": "false",
+#         "output_dir": state.results_path,
+#     })
 
     # Latin Hypercube design, with 10 iters
-    init_rand_samples = int(config["optimizer"].get("init_rand_samples", 10))
-    initial_design = LHDesignWithBiasedSampling
-    initial_design_kwargs = {
-        "init_budget": init_rand_samples,
-        "max_config_fracs": 1,
-    }
+    # init_rand_samples = int(config["optimizer"].get("init_rand_samples", 10))
+    # initial_design = LHDesignWithBiasedSampling
+    # initial_design_kwargs = {
+    #     "init_budget": init_rand_samples,
+    #     "max_config_fracs": 1,
+    # }
 
     # Get RF params from config
-    rand_percentage = float(config["optimizer"]["rand_percentage"])
-    assert 0 <= rand_percentage <= 1, "Optimizer random percentage must be between 0 and 1"
-    n_estimators = int(config["optimizer"]["n_estimators"])
+    # rand_percentage = float(config["optimizer"]["rand_percentage"])
+    # assert 0 <= rand_percentage <= 1, "Optimizer random percentage must be between 0 and 1"
+    # n_estimators = int(config["optimizer"]["n_estimators"])
 
-    #  how often to evaluate a random sample
-    random_configuration_chooser_kwargs = {
-        "prob": rand_percentage,
-    }
+    # #  how often to evaluate a random sample
+    # random_configuration_chooser_kwargs = {
+    #     "prob": rand_percentage,
+    # }
 
-    tae_runner = partial(tae_runner, state=state)
+    # tae_runner = partial(tae_runner, state=state)
 
-    model_type = config["optimizer"].get("model_type", "rf") # default is RF-SMACHPO
-    assert model_type in ["rf", "gp", "mkbo"], "Model type %s not supported" % model_type
+    # model_type = config["optimizer"].get("model_type", "rf") # default is RF-SMACHPO
+    # assert model_type in ["rf", "gp", "mkbo"], "Model type %s not supported" % model_type
 
-    if model_type == "rf":
-        # RF model params -- similar to MLOS ones
-        model_kwargs = {
-            "num_trees": n_estimators,
-            "log_y": False,         # no log scale
-            "ratio_features": 1,    #
-            "min_samples_split": 2, # min number of samples to perform a split
-            "min_samples_leaf": 3,  # min number of smaples on a leaf node
-            "max_depth": 2**20,     # max depth of tree
-        }
-        optimizer = SMAC4HPO(
-            scenario=scenario,
-            tae_runner=tae_runner,
-            rng=config.seed,
-            model_kwargs=model_kwargs,
-            initial_design=initial_design,
-            initial_design_kwargs=initial_design_kwargs,
-            random_configuration_chooser_kwargs=random_configuration_chooser_kwargs,
-        )
+    # if model_type == "rf":
+    #     # RF model params -- similar to MLOS ones
+    #     model_kwargs = {
+    #         "num_trees": n_estimators,
+    #         "log_y": False,         # no log scale
+    #         "ratio_features": 1,    #
+    #         "min_samples_split": 2, # min number of samples to perform a split
+    #         "min_samples_leaf": 3,  # min number of smaples on a leaf node
+    #         "max_depth": 2**20,     # max depth of tree
+    #     }
+    #     optimizer = SMAC4HPO(
+    #         scenario=scenario,
+    #         tae_runner=tae_runner,
+    #         rng=config.seed,
+    #         model_kwargs=model_kwargs,
+    #         initial_design=initial_design,
+    #         initial_design_kwargs=initial_design_kwargs,
+    #         random_configuration_chooser_kwargs=random_configuration_chooser_kwargs,
+    #     )
 
-    elif model_type == "gp":
-        optimizer = SMAC4BB(
-            model_type="gp",
-            scenario=scenario,
-            tae_runner=tae_runner,
-            rng=config.seed,
-            initial_design=initial_design,
-            initial_design_kwargs=initial_design_kwargs,
-            random_configuration_chooser_kwargs=random_configuration_chooser_kwargs,
-        )
+    # elif model_type == "gp":
+    #     optimizer = SMAC4BB(
+    #         model_type="gp",
+    #         scenario=scenario,
+    #         tae_runner=tae_runner,
+    #         rng=config.seed,
+    #         initial_design=initial_design,
+    #         initial_design_kwargs=initial_design_kwargs,
+    #         random_configuration_chooser_kwargs=random_configuration_chooser_kwargs,
+    #     )
 
     # elif model_type == "mkbo":
     #     # OpenBox
@@ -143,5 +145,5 @@ def get_smac_optimizer(config, knob_space: ConfigurationSpace, tae_runner, state
     #         random_state=config.seed,
     #     )
 
-    logger.info(optimizer)
-    return optimizer
+    # logger.info(optimizer)
+    # return optimizer
