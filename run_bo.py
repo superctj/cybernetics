39bf9948f9cfae9df41606c3ef0c48cd0e76a0fe
superctj <congtj@umich.edu>
2024-01-22 04:19:23 +0000

    convert seed from str to int and fix missing inputs in dbms wrapper

--- run_bo.py
+++ run_bo.py
@@ -26,19 +26,27 @@ if __name__ == "__main__":
     # Create workload wrapper
     if config["workload_info"]["framework"] == "benchbase":
         from cybernetics.workload.benchbase import BenchBaseWrapper
+        
         workload_wrapper = BenchBaseWrapper(
             config["workload_info"]["workload"],
-            config["workload_info"]["script"])
+            config["workload_info"]["script"]
+        )
     
     # Create DBMS executor
     if config["dbms_info"]["dbms_name"] == "postgres":
         from cybernetics.dbms_interface.postgres import PostgresWrapper
-        postgres_wrapper = PostgresWrapper(config["dbms_info"], 
-                                           workload_wrapper)
+
+        postgres_wrapper = PostgresWrapper(
+            config["dbms_info"], 
+            workload_wrapper,
+            config["results"]["save_path"]
+        )
 
     # Init DBMS config space
     dbms_config_space_generator = KnobSpaceGenerator(
-        config["knob_space"]["knob_spec"], int(config["knob_space"]["random_seed"]))
+        config["knob_space"]["knob_spec"],
+        int(config["knob_space"]["random_seed"])
+    )
     dbms_config_space = dbms_config_space_generator.generate_input_space(
         ignored_knobs=[])
     
