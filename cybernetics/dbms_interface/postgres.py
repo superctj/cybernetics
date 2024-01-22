1036dca06922a7a0e7f8d732020d7f5ed95704f5
superctj <congtj@umich.edu>
2024-01-22 04:16:22 +0000

    fix the error when failing to restart because of invalid dbms config

--- cybernetics/dbms_interface/postgres.py
+++ cybernetics/dbms_interface/postgres.py
@@ -81,7 +81,7 @@ class PostgresClient(DBClient):
 
 
 class PostgresWrapper:
-    def __init__(self, dbms_info, workload_wrapper=None, results_dir=None) -> None:
+    def __init__(self, dbms_info, workload_wrapper, results_dir: str) -> None:
         self.host = dbms_info["host"]
         self.port = dbms_info["port"]
         self.user = dbms_info["user"]
@@ -95,87 +95,58 @@ class PostgresWrapper:
         self.logger = CUSTOM_LOGGING_INSTANCE.get_module_logger(__name__)
         # TODO: Add support for remote mode
 
-        self.num_metrics = 60
-        self.PG_STAT_VIEWS = [
-            "pg_stat_archiver", "pg_stat_bgwriter",  # global
-            "pg_stat_database", "pg_stat_database_conflicts",  # local
-            "pg_stat_user_tables", "pg_statio_user_tables",  # local
-            "pg_stat_user_indexes", "pg_statio_user_indexes"  # local
-        ]
-        self.PG_STAT_VIEWS_LOCAL_DATABASE = ["pg_stat_database", "pg_stat_database_conflicts"]
-        self.PG_STAT_VIEWS_LOCAL_TABLE = ["pg_stat_user_tables", "pg_statio_user_tables"]
-        self.PG_STAT_VIEWS_LOCAL_INDEX = ["pg_stat_user_indexes", "pg_statio_user_indexes"]
-        self.NUMERIC_METRICS = [
-            # global
-            "buffers_alloc", "buffers_backend", "buffers_backend_fsync", "buffers_checkpoint", "buffers_clean",
-            "checkpoints_req", "checkpoints_timed", "checkpoint_sync_time", "checkpoint_write_time", "maxwritten_clean",
-            "archived_count", "failed_count",
-            # database
-            "blk_read_time", "blks_hit", "blks_read", "blk_write_time", "conflicts", "deadlocks", "temp_bytes",
-            "temp_files", "tup_deleted", "tup_fetched", "tup_inserted", "tup_returned", "tup_updated", "xact_commit",
-            "xact_rollback", "confl_tablespace", "confl_lock", "confl_snapshot", "confl_bufferpin", "confl_deadlock",
-            # table
-            "analyze_count", "autoanalyze_count", "autovacuum_count", "heap_blks_hit", "heap_blks_read", "idx_blks_hit",
-            "idx_blks_read", "idx_scan", "idx_tup_fetch", "n_dead_tup", "n_live_tup", "n_tup_del", "n_tup_hot_upd",
-            "n_tup_ins", "n_tup_upd", "n_mod_since_analyze", "seq_scan", "seq_tup_read", "tidx_blks_hit",
-            "tidx_blks_read",
-            "toast_blks_hit", "toast_blks_read", "vacuum_count",
-            # index
-            "idx_blks_hit", "idx_blks_read", "idx_scan", "idx_tup_fetch", "idx_tup_read"
-        ]
+        # self.num_metrics = 60
+        # self.PG_STAT_VIEWS = [
+        #     "pg_stat_archiver", "pg_stat_bgwriter",  # global
+        #     "pg_stat_database", "pg_stat_database_conflicts",  # local
+        #     "pg_stat_user_tables", "pg_statio_user_tables",  # local
+        #     "pg_stat_user_indexes", "pg_statio_user_indexes"  # local
+        # ]
+        # self.PG_STAT_VIEWS_LOCAL_DATABASE = ["pg_stat_database", "pg_stat_database_conflicts"]
+        # self.PG_STAT_VIEWS_LOCAL_TABLE = ["pg_stat_user_tables", "pg_statio_user_tables"]
+        # self.PG_STAT_VIEWS_LOCAL_INDEX = ["pg_stat_user_indexes", "pg_statio_user_indexes"]
+        # self.NUMERIC_METRICS = [
+        #     # global
+        #     "buffers_alloc", "buffers_backend", "buffers_backend_fsync", "buffers_checkpoint", "buffers_clean",
+        #     "checkpoints_req", "checkpoints_timed", "checkpoint_sync_time", "checkpoint_write_time", "maxwritten_clean",
+        #     "archived_count", "failed_count",
+        #     # database
+        #     "blk_read_time", "blks_hit", "blks_read", "blk_write_time", "conflicts", "deadlocks", "temp_bytes",
+        #     "temp_files", "tup_deleted", "tup_fetched", "tup_inserted", "tup_returned", "tup_updated", "xact_commit",
+        #     "xact_rollback", "confl_tablespace", "confl_lock", "confl_snapshot", "confl_bufferpin", "confl_deadlock",
+        #     # table
+        #     "analyze_count", "autoanalyze_count", "autovacuum_count", "heap_blks_hit", "heap_blks_read", "idx_blks_hit",
+        #     "idx_blks_read", "idx_scan", "idx_tup_fetch", "n_dead_tup", "n_live_tup", "n_tup_del", "n_tup_hot_upd",
+        #     "n_tup_ins", "n_tup_upd", "n_mod_since_analyze", "seq_scan", "seq_tup_read", "tidx_blks_hit",
+        #     "tidx_blks_read",
+        #     "toast_blks_hit", "toast_blks_read", "vacuum_count",
+        #     # index
+        #     "idx_blks_hit", "idx_blks_read", "idx_scan", "idx_tup_fetch", "idx_tup_read"
+        # ]
     
-    # def _start_postgres(self) -> bool:
-    #     proc = subprocess.Popen([self.postgres, f"--config_file={self.pg_cnf}", "-D",  self.pg_data])
-    #     self.pid = proc.pid
+    def _start_postgres(self) -> bool:
+        # This function is only used when failing to restart Postgres due to invalid DBMS configuration (e.g., request too many resources)
+        postgres_auto_config_path = os.path.join(self.db_cluster, "postgresql.auto.conf")
+        with open(postgres_auto_config_path, "w") as f:
+            f.write("# Overwritten invalid configuration.\n")
 
-    #     count = 0
-    #     start_success = True
-    #     self.logger.info("Wait for Postgres connection...")
-
-    #     while True:
-    #         try:
-    #             pg_client = PostgresClient(host=self.host,
-    #                                        port=self.port,
-    #                                        user=self.user,
-    #                                        password=self.password,
-    #                                        db_name=self.db_name)
-    #             if pg_client.conn.closed == 0:
-    #                 self.logger.info("Connected to Postgres.")
-    #                 pg_client.close_connection()
-    #                 break
-    #         except:
-    #             pass
-
-    #         time.sleep(1)
-    #         count += 1
-    #         if count > 600:
-    #             start_success = False
-    #             self.logger.info("Can not connect to Postgres.")
-    #             clear_cmd = """ps -ef|grep postgres|grep -v grep|cut -c 9-15|xargs kill -9"""
-    #             subprocess.Popen(clear_cmd, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, close_fds=True)
-    #             self.logger.info("Kill all Postgres processes.")
-    #             break
-
-    #     self.logger.info(f"Postgres is up! Waited {count} seconds for connection.")
-    #     return start_success
-    
-    # def _kill_postgres(self) -> None:
-    #     kill_cmd = f"{self.pg_ctl} stop -D {self.pg_data}"
-    #     force_kill_cmd1 = "ps aux|grep '" + self.socket + "'|awk '{print $2}'|xargs kill -9"
-    #     force_kill_cmd2 = "ps aux|grep '" + self.pg_cnf + "'|awk '{print $2}'|xargs kill -9"
-
-    #     p_close = subprocess.Popen(kill_cmd, shell=True, stderr=subprocess.STDOUT, stdout=subprocess.PIPE, close_fds=True)
-        
-    #     try:
-    #         _, _ = p_close.communicate(timeout=TIMEOUT_CLOSE)
-    #         ret_code = p_close.poll()
-    #         if ret_code == 0:
-    #             self.logger.info("Close Postgres successfully.")
-    #     except subprocess.TimeoutExpired:
-    #         self.logger.info("Force close Postgres.")
-    #         os.system(force_kill_cmd1)
-    #         os.system(force_kill_cmd2)
-    #         self.logger.info("Postgres is shut down")
+        payload = ["pg_ctl", "-D", self.db_cluster, "-l", self.db_log_filepath, "start"]
+        p = subprocess.Popen(payload, stderr=subprocess.PIPE, 
+                             stdout=subprocess.PIPE, close_fds=True)
+        try:
+            # communicate() will block the program until the subprocess finishes
+            stdout, stderr = p.communicate(timeout=RESTART_TIMEOUT)
+            if p.returncode == 0:
+                self.logger.info("Overwritten invalid configuration and started Postgres with default configuration.")
+                self.logger.info(f"Subprocess output: \n{stdout.decode()}")
+                return True
+            else:
+                self.logger.info("Failed to overwrite invalid configuration and start Postgres with default configuration.")
+                self.logger.info(f"Subprocess output: \n{stderr.decode()}")
+                return False
+        except subprocess.TimeoutExpired:
+            self.logger.info("Timeout when starting Postgres.")
+            return False
 
     def _restart_postgres(self) -> bool:
         payload = ["pg_ctl", "-D", self.db_cluster, "-l", self.db_log_filepath, "restart"]
@@ -251,107 +222,32 @@ class PostgresWrapper:
                 self.logger.info(f"Failed to set knob {k} to {v}.")
                 return False
     
-    # def reset_knob_value(self, k) -> bool:
-    #     """Restore the value of a run-time knob to the default value
-    #     """
-    #     try:
-    #         pg_client = PostgresClient(host=self.host,
-    #                                    port=self.port,
-    #                                    user=self.user,
-    #                                    password=self.password,
-    #                                    db_name=self.db_name,
-    #                                    logger=self.logger)
-    #         reset_sql = f"ALTER SYSTEM RESET {k};"
-    #         reload_sql = "SELECT pg_reload_conf();"
-    #         _ = pg_client.execute(reset_sql)
-    #         _ = pg_client.execute(reload_sql)
-            
-    #         pg_client.close_connection()
-    #         return True
-    #     except:
-    #         self.logger.info(f"Failed to reset knob {k} to the default value.")
-    #         return False
-
-    # def reset_knobs_online(self, knobs: list) -> bool:
-    #     try:
-    #         pg_client = PostgresClient(host=self.host,
-    #                                    port=self.port,
-    #                                    user=self.user,
-    #                                    password=self.password,
-    #                                    db_name=self.db_name)
-    #         for k in knobs:
-    #             self.reset_knob_value(pg_client, k)
-
-    #         pg_client.close_connection()
-    #         self.logger.info(f"[{time.strftime('%Y-%m-%d %H:%M:%S', time.localtime())}] Knobs applied online.")
-
-    #         return True
-    #     except:
-    #         return False
-    
     def apply_knobs(self, knobs: Union[dict, Configuration]) -> bool:
+        if isinstance(knobs, Configuration):
+            knobs = dict(knobs)
+
         try:
             pg_client = PostgresClient(host=self.host,
-                                       port=self.port,
-                                       user=self.user,
-                                       password=self.password,
-                                       db_name=self.db_name,
-                                       logger=self.logger)
-            
-            if isinstance(knobs, Configuration):
-                knobs = dict(knobs)
+                                        port=self.port,
+                                        user=self.user,
+                                        password=self.password,
+                                        db_name=self.db_name,
+                                        logger=self.logger)
 
             for key in knobs.keys():
                 self.set_knob_value(pg_client, key, knobs[key])
-            
+
             pg_client.close_connection()
-            self._restart_postgres()
-            self.logger.info("Knobs applied.")
-            return True
         except:
-            self.logger.info("Failed to apply knobs.")
+            self.logger.info("Failed to alter knobs.")
             return False
-    
-    # def reset_runtime_knobs(self) -> bool:
-    #     try:
-    #         pg_client = PostgresClient(host=self.host,
-    #                                    port=self.port,
-    #                                    user=self.user,
-    #                                    password=self.password,
-    #                                    db_name=self.db_name,
-    #                                    logger=self.logger)
-            
-    #         reset_sql = "RESET ALL;"
-    #         _ = pg_client.execute(reset_sql)
-    #         pg_client.close_connection()
 
-    #         self.logger.info(f"Reset runtime knobs.")
-    #         return True
-    #     except:
-    #         self.logger.info("Failed to reset runtime knobs.")
-    #         return False
-    
-    # def reset_knobs_by_reloading_config(self) -> bool:
-    #     try:
-    #         pg_client = PostgresClient(host=self.host,
-    #                                    port=self.port,
-    #                                    user=self.user,
-    #                                    password=self.password,
-    #                                    db_name=self.db_name,
-    #                                    logger=self.logger)
-            
-    #         reset_sql = "ALTER SYSTEM RESET ALL;"
-    #         reload_sql = "SELECT pg_reload_conf();"
-
-    #         _ = pg_client.execute(reset_sql)
-    #         _ = pg_client.execute(reload_sql)
-    #         pg_client.close_connection()
-
-    #         self.logger.info(f"Reset runtime knobs.")
-    #         return True
-    #     except:
-    #         self.logger.info("Failed to reset runtime knobs.")
-    #         return False
+        restart_predicate = self._restart_postgres()
+        if restart_predicate:
+            self.logger.info("Restarted Postgres to make the DBMS config take effect.")
+            return True
+        else:
+            return self._start_postgres()
     
     def reset_knobs_by_restarting_db(self) -> bool:
         try:
@@ -373,38 +269,6 @@ class PostgresWrapper:
             self.logger.info("Failed to reset knobs with Postgres restarted.")
             return False
 
-    # def apply_knobs_offline(self, knobs: dict) -> bool:
-    #     self._kill_postgres()
-
-    #     if "min_wal_size" in knobs.keys():
-    #         if "wal_segment_size" in knobs.keys():
-    #             wal_segment_size = knobs["wal_segment_size"]
-    #         else:
-    #             wal_segment_size = 16
-            
-    #         if knobs["min_wal_size"] < 2 * wal_segment_size:
-    #             knobs["min_wal_size"] = 2 * wal_segment_size
-    #             self.logger.info("min_wal_size must be at least twice wal_segment_size")
-
-    #     # knobs_not_in_cnf = self._gen_config_file(knobs)
-        
-    #     success = self._start_postgres()
-    #     try:
-    #         self.logger.info(f"Sleep for {RESTART_TIMEOUT} seconds after restarting Postgres...")
-    #         time.sleep(RESTART_TIMEOUT)
-
-    #         self.apply_knobs_online(knobs)
-
-    #         # if len(knobs_not_in_cnf) > 0:
-    #         #     tmp_rds = {}
-    #         #     for knob_rds in knobs_not_in_cnf:
-    #         #         tmp_rds[knob_rds] = knobs[knob_rds]
-    #         #     self.apply_knobs_online(tmp_rds)
-    #     except:
-    #         success = False
-
-    #     return success
-
     def get_benchbase_metrics(self):
         metrics_files = glob.glob(f"{self.results_dir}/*.summary.json")
         latest_metrics_file = max(metrics_files, key=os.path.getctime)
@@ -413,17 +277,3 @@ class PostgresWrapper:
             metrics = json.load(f)
         
         return metrics
-
-    # def evaluate_db_info(self, knobs: dict) -> dict:
-    #     success = self.apply_knobs_offline(knobs)
-    #     if not success:
-    #         self.logger.info("Failed to apply the given DBMS configuration.")
-    #         return {}
-        
-    #     # Execute the workload
-    #     self.workload_wrapper.run()
-
-    #     # Collect metrics
-    #     metrics = self._get_benchbase_metrics()
-
-    #     return metrics
