d5b13520f6af61469b0b7b64a8be9512aea81640
superctj <congtj@umich.edu>
2024-02-05 04:35:07 +0000

    add placeholder of rl-ddpg optimizer

--- cybernetics/tuning/dbms_config_optimizer.py
+++ cybernetics/tuning/dbms_config_optimizer.py
@@ -1,6 +1,6 @@
 """DBMS configuration optimizers.
 
-https://github.com/uw-mad-dash/llamatune/blob/main/optimizer.py
+Adapted from https://github.com/uw-mad-dash/llamatune/blob/main/optimizer.py
 """
 
 from functools import partial
@@ -42,101 +42,192 @@ def get_bo_optimizer(config, dbms_config_space: ConfigurationSpace, target_funct
     return optimizer
 
 
-# def get_smac_optimizer(config, knob_space: ConfigurationSpace, tae_runner, state, ignored_knobs=None, run_history=None):
-#     logger = CUSTOM_LOGGING_INSTANCE.get_module_logger(__name__)
-#     logger.info("Constructing new optimizer...")
-#     logger.info(f"Run History: {run_history}")
-#     logger.info(f"Ignored knobs: {ignored_knobs}")
+def get_ddpg_optimizer(config, dbms_config_space: ConfigurationSpace, target_function, state):
+    pass
+    # random number generator
+    # rng = np.random.RandomState(seed=config.seed)
 
-#     # Generate input (i.e. knobs) and output (i.e. perf metric) space
-#     input_space = knob_space.generate_input_space(
-#         config.seed, ignore_extra_knobs=ignored_knobs)
+    # from smac.stats.stats import Stats
+    # from smac.utils.io.traj_logging import TrajLogger
+    # from smac.utils.io.output_directory import create_output_directory
 
-#     logger.info(f"\ninput space:\n{input_space}")
+    # scenario = Scenario({
+    #     "run_obj": "quality",
+    #     "runcount-limit": config.iters,
+    #     "cs": dbms_config_space,
+    #     "deterministic": "true",
+    #     "always_race_default": "false",
+    #     # disables pynisher, which allows for shared state
+    #     "limit_resources": "false",
+    #     "output_dir": state.results_path,
+    # })
 
-#     scenario = Scenario({
-#         "run_obj": "quality",
-#         "runcount-limit": config.iters,
-#         "cs": input_space,
-#         "deterministic": "true",
-#         "always_race_default": "false",
-#         # disables pynisher, which allows for shared state
-#         "limit_resources": "false",
-#         "output_dir": state.results_path,
-#     })
+    # output_dir = create_output_directory(scenario, 0)
+    # stats = Stats(scenario)
+    # traj_logger = TrajLogger(output_dir=output_dir, stats=stats)
 
-    # Latin Hypercube design, with 10 iters
+    # # Latin Hypercube design, with 10 iters
     # init_rand_samples = int(config["optimizer"].get("init_rand_samples", 10))
-    # initial_design = LHDesignWithBiasedSampling
-    # initial_design_kwargs = {
-    #     "init_budget": init_rand_samples,
+    # init_design_def_kwargs = {
+    #     "cs": input_space,
+    #     "rng": rng,
+    #     "traj_logger": traj_logger, # required
+    #     "ta_run_limit": 99999999999,
     #     "max_config_fracs": 1,
+    #     "init_budget": init_rand_samples,
     # }
+    # initial_design = LHDesignWithBiasedSampling(**init_design_def_kwargs)
 
-    # Get RF params from config
+    # Random conf chooser
+    # from smac.optimizer.random_configuration_chooser import ChooserProb
+
     # rand_percentage = float(config["optimizer"]["rand_percentage"])
-    # assert 0 <= rand_percentage <= 1, "Optimizer random percentage must be between 0 and 1"
-    # n_estimators = int(config["optimizer"]["n_estimators"])
+    # assert 0 <= rand_percentage <= 1, "Optimizer rand optimizer must be between 0 and 1"
+    # rcc_rng = np.random.RandomState(seed=config.seed)
+    # rand_conf_chooser = ChooserProb(rcc_rng, rand_percentage)
 
-    # #  how often to evaluate a random sample
-    # random_configuration_chooser_kwargs = {
-    #     "prob": rand_percentage,
-    # }
+    # DDPG Model
+    # from cybernetics.tuning.ddpg.model import DDPG
+    # n_states = config.num_dbms_metrics
+    # n_actions = len(input_space)
+
+    # model = DDPG(n_states, n_actions, model_name="ddpg_model")
 
     # tae_runner = partial(tae_runner, state=state)
+    # optimizer = DDPGOptimizer(state, model, tae_runner, initial_design, rand_conf_chooser, config.iters, logging_dir=state.results_path)
 
-    # model_type = config["optimizer"].get("model_type", "rf") # default is RF-SMACHPO
-    # assert model_type in ["rf", "gp", "mkbo"], "Model type %s not supported" % model_type
-
-    # if model_type == "rf":
-    #     # RF model params -- similar to MLOS ones
-    #     model_kwargs = {
-    #         "num_trees": n_estimators,
-    #         "log_y": False,         # no log scale
-    #         "ratio_features": 1,    #
-    #         "min_samples_split": 2, # min number of samples to perform a split
-    #         "min_samples_leaf": 3,  # min number of smaples on a leaf node
-    #         "max_depth": 2**20,     # max depth of tree
-    #     }
-    #     optimizer = SMAC4HPO(
-    #         scenario=scenario,
-    #         tae_runner=tae_runner,
-    #         rng=config.seed,
-    #         model_kwargs=model_kwargs,
-    #         initial_design=initial_design,
-    #         initial_design_kwargs=initial_design_kwargs,
-    #         random_configuration_chooser_kwargs=random_configuration_chooser_kwargs,
-    #     )
-
-    # elif model_type == "gp":
-    #     optimizer = SMAC4BB(
-    #         model_type="gp",
-    #         scenario=scenario,
-    #         tae_runner=tae_runner,
-    #         rng=config.seed,
-    #         initial_design=initial_design,
-    #         initial_design_kwargs=initial_design_kwargs,
-    #         random_configuration_chooser_kwargs=random_configuration_chooser_kwargs,
-    #     )
-
-    # elif model_type == "mkbo":
-    #     # OpenBox
-    #     import openbox
-    #     # openbox.utils.limit._platform = "Windows" # Patch to avoid objective function wrapping
-    #     optimizer = openbox.Optimizer(
-    #         tae_runner,
-    #         input_space,
-    #         num_objs=1,
-    #         num_constraints=0,
-    #         max_runs=config.iters,
-    #         surrogate_type="gp",
-    #         acq_optimizer_type="local_random",
-    #         initial_runs=10,
-    #         init_strategy="random_explore_first",
-    #         time_limit_per_trial=10**6,
-    #         logging_dir=state.results_path,
-    #         random_state=config.seed,
-    #     )
-
-    # logger.info(optimizer)
     # return optimizer
+
+
+class DDPGOptimizer:
+    def __init__(self, state, model, func, initial_design, rand_conf_chooser,
+                    n_iters, logging_dir=None):
+        assert state.target_metric == "throughput"
+
+        self.state = state
+        self.model = model
+        self.func = func
+        self.initial_design = initial_design
+        self.input_space = initial_design.cs
+        self.rand_conf_chooser = rand_conf_chooser
+        self.n_iters = n_iters
+        self.n_epochs = 2 # same value as CDBTune
+
+        self.logging_dir = logging_dir
+
+    def run(self):
+        prev_perf = self.state.default_perf
+        assert prev_perf >= 0
+
+        results = [ ]
+
+        # Bootstrap with random samples
+        init_configurations = self.initial_design.select_configurations()
+        for i, knob_data in enumerate(init_configurations):
+            print(f"Iter {i} -- RANDOM")
+            ### reward, metric_data = env.simulate(knob_data)
+            # metrics & perf
+            perf, metric_data = self.func(knob_data)
+            perf = -perf # maximize
+            assert perf >= 0
+            # compute reward
+            reward = self.get_reward(perf, prev_perf)
+            # LOG
+            print(f"Iter {i} -- PERF = {perf}")
+            print(f"Iter {i} -- METRICS = {metric_data}")
+            print(f"Iter {i} -- REWARD = {reward}")
+
+            if i > 0:
+                self.model.add_sample(prev_metric_data, prev_knob_data, prev_reward, metric_data)
+
+            prev_metric_data = metric_data
+            prev_knob_data = knob_data.get_array() # scale to [0, 1]
+            prev_reward = reward
+            prev_perf = perf
+
+        # add last random sample
+        self.model.add_sample(prev_metric_data, prev_knob_data, prev_reward, metric_data)
+
+        # Start guided search
+        for i in range(len(init_configurations), self.n_iters):
+
+            if self.rand_conf_chooser.check(i):
+                print(f"Iter {i} -- RANDOM SAMPLE")
+                # get random sample from config space
+                knob_data = self.input_space.sample_configuration()
+                knob_data_vector = knob_data.get_array()
+            else:
+                print(f"Iter {i} -- GUIDED")
+                # get next recommendation from DDPG
+                knob_data_vector = self.model.choose_action(prev_metric_data)
+                knob_data = self.convert_from_vector(knob_data_vector)
+
+            # metrics & perf
+            perf, metric_data = self.func(knob_data)
+            perf = -perf # maximize
+            assert perf >= 0
+            # compute reward
+            reward = self.get_reward(perf, prev_perf)
+            # LOG
+            print(f"Iter {i} -- PERF = {perf}")
+            print(f"Iter {i} -- METRICS = {metric_data}")
+            print(f"Iter {i} -- REWARD = {reward}")
+
+            # register point to the optimizer
+            self.model.add_sample(prev_metric_data, prev_knob_data, prev_reward, metric_data)
+
+            prev_metric_data = metric_data
+            prev_knob_data = knob_data_vector
+            prev_reward = reward
+            prev_perf = perf
+
+            # update DDPG model
+            if len(self.model.replay_memory) >= self.model.batch_size:
+                for _ in range(self.n_epochs):
+                    self.model.update()
+            print("AFTER UPDATE")
+
+    def get_reward(self, perf, prev_perf):
+        """ Reward calculation same as CDBTune paper -- Section 4.2 """
+        def calculate_reward(delta_default, delta_prev):
+            if delta_default > 0:
+                reward =   ((1 + delta_default) ** 2 - 1) * np.abs(1 + delta_prev)
+            else:
+                reward = - ((1 - delta_default) ** 2 - 1) * np.abs(1 - delta_prev)
+
+            # no improvement over last evaluation -- 0 reward
+            if reward > 0 and delta_prev < 0:
+                reward = 0
+
+            return reward
+
+        if perf == self.state.worse_perf:
+            return 0
+
+        # perf diff from default / prev evaluation
+        delta_default = (perf - self.state.default_perf) / self.state.default_perf
+        delta_prev = (perf - prev_perf) / prev_perf
+
+        return calculate_reward(delta_default, delta_prev)
+
+    # def convert_from_vector(self, vector):
+    #     from adapters.bias_sampling import special_value_scaler, UniformIntegerHyperparameterWithSpecialValue
+
+    #     values = { }
+    #     for hp, value in zip(self.input_space.get_hyperparameters(), vector):
+    #         if isinstance(hp, CSH.NumericalHyperparameter):
+    #             if isinstance(hp, UniformIntegerHyperparameterWithSpecialValue):
+    #                 value = special_value_scaler(hp, value)
+    #             else:
+    #                 value = hp._transform(value)
+    #         elif isinstance(hp, CSH.CategoricalHyperparameter):
+    #             assert hp.num_choices == 2
+    #             value = hp.choices[0] if value <= 0.5 else hp.choices[1]
+    #         else:
+    #             raise NotImplementedError()
+
+    #         values[hp.name] = value
+
+    #     conf = CS.Configuration(self.input_space, values=values)
+    #     self.input_space.check_configuration(conf)
+    #     return conf
