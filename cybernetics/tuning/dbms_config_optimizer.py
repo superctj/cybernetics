ba5ee37359fb3f44fbe415beb9fb3679abf3a153
superctj <congtj@umich.edu>
2024-02-06 01:00:16 +0000

    remove random config chooser

--- cybernetics/tuning/dbms_config_optimizer.py
+++ cybernetics/tuning/dbms_config_optimizer.py
@@ -5,6 +5,7 @@ Adapted from https://github.com/uw-mad-dash/llamatune/blob/main/optimizer.py
 
 from functools import partial
 
+import ConfigSpace.hyperparameters as CSH
 import numpy as np
 import smac.initial_design as smac_init_design
 
@@ -13,7 +14,6 @@ from smac import BlackBoxFacade as BBFacade
 from smac import HyperparameterOptimizationFacade as HPOFacade
 from smac import Scenario
 
-# from cybernetics.knobs.bias_sampling import LHDesignWithBiasedSampling
 from cybernetics.utils.custom_logging import CUSTOM_LOGGING_INSTANCE
 
 
@@ -47,7 +47,7 @@ def get_bo_optimizer(config, dbms_config_space: ConfigurationSpace,
 
 
 def get_ddpg_optimizer(config, dbms_config_space: ConfigurationSpace,
-                       target_function, state):
+                       target_function, exp_state):
     scenario = Scenario(
         configspace=dbms_config_space,
         output_directory=config["results"]["save_path"],
@@ -64,13 +64,6 @@ def get_ddpg_optimizer(config, dbms_config_space: ConfigurationSpace,
             seed=config["knob_space"]["random_seed"]
         )
 
-    # Random conf chooser
-    # from smac.optimizer.random_configuration_chooser import ChooserProb
-    # rand_percentage = float(config["optimizer"]["rand_percentage"])
-    # assert 0 <= rand_percentage <= 1, "Optimizer rand optimizer must be between 0 and 1"
-    # rcc_rng = np.random.RandomState(seed=config.seed)
-    # rand_conf_chooser = ChooserProb(rcc_rng, rand_percentage)
-
     # DDPG Model
     from cybernetics.tuning.ddpg.model import DDPG
     
@@ -80,16 +73,15 @@ def get_ddpg_optimizer(config, dbms_config_space: ConfigurationSpace,
 
     target_function = partial(target_function,
                               seed=int(config["knob_space"]["random_seed"]))
-    optimizer = DDPGOptimizer(state, model, target_function, initial_design,
-                              rand_conf_chooser,
+    optimizer = DDPGOptimizer(exp_state, model, target_function, initial_design,
                               config["config_optimizer"]["n_total_configs"])
 
     return optimizer
 
 
 class DDPGOptimizer:
-    def __init__(self, exp_state, model, target_function, initial_design, 
-                 rand_conf_chooser, n_iters: int, n_epochs: int=2):
+    def __init__(self, exp_state, model, target_function, initial_design,
+                 n_iters: int, n_epochs: int=2):
         assert exp_state._target_metric == "throughput"
 
         self.exp_state = exp_state
@@ -97,7 +89,6 @@ class DDPGOptimizer:
         self.target_function = target_function
         self.initial_design = initial_design
         self.input_space = initial_design.cs
-        self.rand_conf_chooser = rand_conf_chooser
         self.n_iters = n_iters
         self.n_epochs = n_epochs # CDBTune uses 2
         self.logger = CUSTOM_LOGGING_INSTANCE.get_logger()
@@ -110,7 +101,7 @@ class DDPGOptimizer:
         init_configurations = self.initial_design.select_configurations()
 
         for i, dbms_config in enumerate(init_configurations):
-            self.logger.info(f"Initial Design -- Iter {i}:")
+            self.logger.info(f"Iter {i} -- Sample from Initial Design:")
 
             perf, internal_metrics = self.target_function(dbms_config)
             perf = -perf # maximize
@@ -137,17 +128,12 @@ class DDPGOptimizer:
 
         # Start guided search
         for i in range(len(init_configurations), self.n_iters):
-            if self.rand_conf_chooser.check(i):
-                self.logger.info(f"Guided Search -- Iter {i} -- RANDOM:")
-                # get random sample from config space
-                dbms_config = self.input_space.sample_configuration()
-            else:
-                self.logger.info(f"Guided Search -- Iter {i} -- DDPG:")
-                # get next recommendation from DDPG
-                dbms_config = self.model.choose_action(prev_internal_metrics)
+            self.logger.info(f"Iter {i} -- Sample from DDPG:")
+            # get next recommendation from DDPG
+            dbms_config = self.model.choose_action(prev_internal_metrics)
 
             # metrics & perf
-            perf, metric_data = self.target_function(dbms_config)
+            perf, internal_metrics = self.target_function(dbms_config)
             perf = -perf # maximize
             assert perf >= 0
             # compute reward
@@ -195,24 +181,21 @@ class DDPGOptimizer:
 
         return calculate_reward(delta_default, delta_prev)
 
-    # def convert_from_vector(self, vector):
-    #     from adapters.bias_sampling import special_value_scaler, UniformIntegerHyperparameterWithSpecialValue
+    def convert_model_outputs_to_dbms_config(self, model_outputs) -> dict:
+        dbms_config = {}
 
-    #     values = { }
-    #     for hp, value in zip(self.input_space.get_hyperparameters(), vector):
-    #         if isinstance(hp, CSH.NumericalHyperparameter):
-    #             if isinstance(hp, UniformIntegerHyperparameterWithSpecialValue):
-    #                 value = special_value_scaler(hp, value)
-    #             else:
-    #                 value = hp._transform(value)
-    #         elif isinstance(hp, CSH.CategoricalHyperparameter):
-    #             assert hp.num_choices == 2
-    #             value = hp.choices[0] if value <= 0.5 else hp.choices[1]
-    #         else:
-    #             raise NotImplementedError()
+        # TODO: Need to double check if model_outputs matches the input space
+        for hp, value in zip(self.input_space.get_hyperparameters(), model_outputs):
+            if isinstance(hp, CSH.CategoricalHyperparameter):
+                if hp.num_choices == 2:
+                    value = hp.choices[0] if value <= 0.5 else hp.choices[1]
+                else:
+                    raise NotImplementedError()
+            elif isinstance(hp, CSH.UniformIntegerHyperparameter) or isinstance(hp, CSH.UniformFloatHyperparameter):
+                value = hp._transform(value)
+            else:
+                raise NotImplementedError()
 
-    #         values[hp.name] = value
+            dbms_config[hp.name] = value
 
-    #     conf = CS.Configuration(self.input_space, values=values)
-    #     self.input_space.check_configuration(conf)
-    #     return conf
+        return dbms_config
