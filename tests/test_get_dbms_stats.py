39ebab552ddba27edce3a99a18591b5c670734ab
superctj <congtj@umich.edu>
2024-02-08 03:31:39 +0000

    test latest api

--- tests/test_get_dbms_stats.py
+++ tests/test_get_dbms_stats.py
@@ -9,17 +9,19 @@ def test_get_dbms_stats():
         "password": "12345",
         "db_name": "benchbase_tpcc",
         "db_cluster": "/data/pgsql/data",
-        "db_log_filepath": "/data/pgsql/log"
+        "db_log_filepath": "/data/pgsql/log",
+        "n_internal_stats": 25
     }
 
     postgres_wrapper = PostgresWrapper(dbms_info,
                                        workload_wrapper=None,
                                        results_dir=None)
     
-    dbms_stats = postgres_wrapper.get_dbms_stats()
+    numeric_stats, dbms_stats = postgres_wrapper.get_dbms_stats()
     assert len(dbms_stats) == len(postgres_wrapper.DB_STATS_VIEWS) + \
                               len(postgres_wrapper.CLUSTER_STATS_VIEWS)
     assert "pg_stat_database" in dbms_stats
     assert dbms_stats["pg_stat_database"]["datname"] == dbms_info["db_name"]
     assert "blks_read" in dbms_stats["pg_stat_database"]
     assert "buffers_alloc" in dbms_stats["pg_stat_bgwriter"]
+    assert numeric_stats.shape[0] == dbms_info["n_internal_stats"]
