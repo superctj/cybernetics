98a4c1bb3b581fca008a7a94f1fe0bb352840a6b
Damian Zhang <zzyDipper@gmail.com>
2024-03-29 19:15:35 -0700

    Decouple, avoid loading data set each time

--- cybernetics/tuning/engine.py
+++ cybernetics/tuning/engine.py
@@ -6,7 +6,7 @@ https://github.com/PKU-DAIR/DBTune/blob/main/autotune/tuner.py
 https://github.com/uw-mad-dash/llamatune/blob/main/run-smac.py
 """
 
-from cybernetics.tuning.dbms_config_optimizer import get_bo_optimizer, get_ddpg_optimizer,get_liquid_ddpg_optimizer
+from cybernetics.tuning.dbms_config_optimizer import get_bo_optimizer, get_ddpg_optimizer
 from cybernetics.utils.custom_logging import CUSTOM_LOGGING_INSTANCE
 from cybernetics.utils.exp_tracker import ExperimentState
 import time
@@ -112,16 +112,7 @@ class TuningEngine:
                 self.rl_target_function,
                 self.exp_state
             )
-        elif self.config["config_optimizer"]["optimizer"].startswith("liquid"):
-            self.logger.info("Initiating RL-based optimizer...")
-
-            optimizer = get_liquid_ddpg_optimizer(
-                self.config,
-                self.dbms_config_space,
-                self.rl_target_function,
-                self.exp_state
-            )
-
+        
         else:
             raise ValueError(f"Optimizer {optimizer} not supported.")
 
