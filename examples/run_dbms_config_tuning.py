448fbc4a110eff2c9b868683296641d54693766f
superctj <congtj@umich.edu>
2024-05-29 05:42:16 +0000

    separate examples of default tuning and tuning with dimension reduction

---
+++ examples/run_dbms_config_tuning.py
@@ -0,0 +1,67 @@
+import argparse
+
+from cybernetics.tuning.engine import TuningEngine
+from cybernetics.knobs.generate_space import KnobSpaceGenerator
+from cybernetics.utils.util import (
+    fix_global_random_state,
+    get_postgres_user_and_password,
+    parse_config,
+)
+
+
+if __name__ == "__main__":
+    # Parsing command line arguments
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument(
+        "--config_path",
+        type=str,
+        required=True,
+        help="Path to the configuration file",
+    )
+
+    args = parser.parse_args()
+
+    # Parse configuration
+    config = parse_config(args.config_path)
+
+    # Set global random state
+    fix_global_random_state(int(config["knob_space"]["random_seed"]))
+
+    # Create workload wrapper
+    if config["workload_info"]["framework"] == "benchbase":
+        from cybernetics.workload.benchbase import BenchBaseWrapper
+
+        workload_wrapper = BenchBaseWrapper(
+            config["workload_info"]["workload"],
+            config["workload_info"]["script"],
+        )
+
+    # Create DBMS executor
+    if config["dbms_info"]["dbms_name"] == "postgres":
+        from cybernetics.dbms_interface.postgres import PostgresWrapper
+
+        postgres_user, postgres_password = get_postgres_user_and_password()
+        config["dbms_info"]["user"] = postgres_user
+        config["dbms_info"]["password"] = postgres_password
+
+        postgres_wrapper = PostgresWrapper(
+            config["dbms_info"],
+            workload_wrapper,
+            config["results"]["save_path"],
+        )
+
+    # Init DBMS config space
+    dbms_config_space_generator = KnobSpaceGenerator(
+        config["knob_space"]["knob_spec"],
+        int(config["knob_space"]["random_seed"]),
+    )
+    dbms_config_space = dbms_config_space_generator.generate_input_space(
+        ignored_knobs=[]
+    )
+
+    # Init tuning engine
+    tuning_engine = TuningEngine(
+        config, postgres_wrapper, dbms_config_space, workload_wrapper
+    )
+    tuning_engine.run()
