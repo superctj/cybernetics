31926f27038ac7dafa6757bbe623e2b0516ee190
superctj <congtj@umich.edu>
2024-05-30 06:33:35 +0000

    fix tests

--- tests/test_config.py
+++ tests/test_config.py
@@ -5,8 +5,10 @@ from cybernetics.utils.util import parse_config
 
 def test_config():
     root_dir = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
-    config_path = os.path.join(root_dir, "cybernetics/configs/benchbase/tpcc/postgres.ini")
-    
+    config_path = os.path.join(
+        root_dir, "cybernetics/configs/benchbase/tpcc/postgres_bo_gp.ini"
+    )
+
     parser = parse_config(config_path)
     assert "dbms_info" in parser.sections()
     assert parser["dbms_info"]["dbms_name"] in ["postgres", "mysql"]
--- tests/test_dbms_config_generator.py
+++ tests/test_dbms_config_generator.py
@@ -6,13 +6,18 @@ from cybernetics.utils.util import get_proj_dir
 
 def test_dbms_config_generator():
     proj_dir = get_proj_dir(__file__, file_level=2)
-    knob_spec_path = os.path.join(proj_dir,
-                                  "cybernetics/knobs/postgres_12.17_selected_knobs.json")
+    knob_spec_path = os.path.join(
+        proj_dir, "cybernetics/knobs/postgres_12.17_selected_knobs.json"
+    )
     assert os.path.exists(knob_spec_path)
     random_seed = 12345
 
-    dbms_config_space_generator = KnobSpaceGenerator(knob_spec_path, random_seed)
-    dbms_config_space = dbms_config_space_generator.generate_input_space(ignored_knobs=[])
+    dbms_config_space_generator = KnobSpaceGenerator(
+        knob_spec_path, random_seed
+    )
+    dbms_config_space = dbms_config_space_generator.generate_input_space(
+        ignored_knobs=[]
+    )
 
     if "work_mem" in dbms_config_space:
         work_mem = dbms_config_space["work_mem"]
@@ -20,7 +25,10 @@ def test_dbms_config_generator():
         print(f"\nType of dbms_config_space: {type(dbms_config_space)}")
 
         dbms_config_sample = dbms_config_space.sample_configuration()
-        assert dbms_config_sample["work_mem"] >= min_val and dbms_config_sample["work_mem"] <= max_val
+        assert (
+            dbms_config_sample["work_mem"] >= min_val
+            and dbms_config_sample["work_mem"] <= max_val
+        )
         print(f"Type of dbms_config_sample: {type(dbms_config_sample)}")
 
         print(f"\nwork_mem value range: {min_val} - {max_val}")
