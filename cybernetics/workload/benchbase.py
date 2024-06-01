a02b38b201e8c38a918527da84c585f549c5ed3f
superctj <congtj@umich.edu>
2024-06-01 05:59:57 +0000

    encapsulate benchbase execution commands

--- cybernetics/workload/benchbase.py
+++ cybernetics/workload/benchbase.py
@@ -1,25 +1,112 @@
+import os
 import subprocess
 
 from cybernetics.utils.custom_logging import CUSTOM_LOGGING_INSTANCE
 
 
-TIMEOUT = 36000 # 1 hour
+TIMEOUT = 36000  # 1 hour
 
 
 class BenchBaseWrapper:
-    def __init__(self, workload: str, script: str) -> None:
+    def __init__(
+        self,
+        target_dir: str,
+        dbms_name: str,
+        workload: str,
+        results_save_dir: str = None,
+    ) -> None:
+        self.target_dir = target_dir
+        self.dbms_name = dbms_name
         self.workload = workload
-        self.script = script
+        self.results_save_dir = results_save_dir
+
+        self.first_run = True
         self.logger = CUSTOM_LOGGING_INSTANCE.get_logger()
 
     def run(self) -> None:
-        payload = ["bash", self.script]
-        workload_process = subprocess.Popen(payload, stderr=subprocess.PIPE, 
-                                            stdout=subprocess.PIPE, close_fds=True)
+        workload_config_path = (
+            f"./config/{self.dbms_name}/sample_{self.workload}_config.xml"
+        )
+
+        # Load data in the first run
+        if self.first_run:
+            # Changing the current working directory may have a side effect
+            os.chdir(self.target_dir)
+
+            # Save the results to a specified directory
+            if self.results_save_dir:
+                payload = [
+                    "java",
+                    "-jar",
+                    "benchbase.jar",
+                    "-b",
+                    self.workload,
+                    "-c",
+                    workload_config_path,
+                    "-d",
+                    self.results_save_dir,
+                    "--create=true",
+                    "--load=true",
+                    "--execute=true",
+                ]
+            # Save the results to the BenchBase default directory
+            else:
+                payload = [
+                    "java",
+                    "-jar",
+                    "benchbase.jar",
+                    "-b",
+                    self.workload,
+                    "-c",
+                    workload_config_path,
+                    "--create=true",
+                    "--load=true",
+                    "--execute=true",
+                ]
+
+            self.first_run = False
+        # Skip loading data in the subsequent runs
+        else:
+            if self.results_save_dir:
+                payload = [
+                    "java",
+                    "-jar",
+                    "benchbase.jar",
+                    "-b",
+                    self.workload,
+                    "-c",
+                    workload_config_path,
+                    "-d",
+                    self.results_save_dir,
+                    "--create=false",
+                    "--load=false",
+                    "--execute=true",
+                ]
+            else:
+                payload = [
+                    "java",
+                    "-jar",
+                    "benchbase.jar",
+                    "-b",
+                    self.workload,
+                    "-c",
+                    workload_config_path,
+                    "--create=false",
+                    "--load=false",
+                    "--execute=true",
+                ]
+
+        workload_process = subprocess.Popen(
+            payload,
+            stderr=subprocess.PIPE,
+            stdout=subprocess.PIPE,
+            close_fds=True,
+        )
 
         try:
             # communicate() will block the program until the subprocess finishes
             stdout, stderr = workload_process.communicate(timeout=TIMEOUT)
+
             if workload_process.returncode == 0:
                 self.logger.info("Finish running workload.")
                 self.logger.info(f"Subprocess output: \n{stdout.decode()}")
@@ -28,11 +115,3 @@ class BenchBaseWrapper:
                 self.logger.info(f"Subprocess output: \n{stderr.decode()}")
         except subprocess.TimeoutExpired:
             self.logger.info("Timeout when running workload.")
-
-
-if __name__ == "__main__":
-    workload="tpcc"
-    script="/home/tianji/cybernetics/scripts/benchbase_tpcc_postgres.sh"
-
-    benchbase_wrapper = BenchBaseWrapper(workload, script)
-    benchbase_wrapper.run()
